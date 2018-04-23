f3199271eced519de9ccedf16333faa55477dd06
Steven Ng <steven.ng@temple.edu>
2018-04-23 15:30:12 -0400

    Restore building factory

---
+++ spec/factories/buildings.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :building do
+    name "Charles Samuel Addams Library"
+    description "Main Campus Main Library"
+    address1 "1250 Polett Walk"
+    temple_building_code "ABC"
+    directions_map "http://maps.exammple.edu/CSA.jpg"
+    hours "Always Open"
+    phone_number "2155551212"
+    image "https://i.pinimg.com/736x/c8/51/2f/c8512fd8b92570627bbd35f44171d44d.jpg"
+    campus "Main Campus"
+    accessibility "Yes"
+    email "csa@example.edu"
+  end
+end

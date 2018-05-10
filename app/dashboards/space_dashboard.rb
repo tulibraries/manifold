758d0ac321de98351069ce625a7e0916b3b49023
Chris Doyle <cdoyle@temple.edu>
2018-05-10 10:34:34 -0400

    Added administrate gem and generated backend for buildings and spaces.

---
+++ app/dashboards/space_dashboard.rb
@@ -0,0 +1,78 @@
+require "administrate/base_dashboard"
+
+class SpaceDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    name: Field::String,
+    description: Field::Text,
+    hours: Field::String,
+    accessibility: Field::Text,
+    location: Field::String,
+    phone_number: Field::String,
+    image: Field::String,
+    email: Field::String,
+    building_id: Field::Number,
+    space_id: Field::Number,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :id,
+    :name,
+    :description,
+    :hours,
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :id,
+    :name,
+    :description,
+    :hours,
+    :accessibility,
+    :location,
+    :phone_number,
+    :image,
+    :email,
+    :building_id,
+    :space_id,
+    :created_at,
+    :updated_at,
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :name,
+    :description,
+    :hours,
+    :accessibility,
+    :location,
+    :phone_number,
+    :image,
+    :email,
+    :building_id,
+    :space_id,
+  ].freeze
+
+  # Overwrite this method to customize how spaces are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(space)
+  #   "Space ##{space.id}"
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :buildings
+      resources :spaces
+
+      root to: "buildings#index"
+    end
   resources :buildings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

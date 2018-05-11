c74d34ea566ad469741a650b4d350ba7010ca013
Steven Ng <steven.ng@temple.edu>
2018-05-11 10:52:41 -0400

    Uncomment building and space validates

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,8 +4,8 @@ class Person < ApplicationRecord
   validates :first_name, :last_name, :chat_handle, :job_title, :identifier, presence: true
  	validates :email_address, presence: true, email: true
  	validates :phone_number, presence: true, phone_number: true
-# 	validates :building_id, presence: true, valid_building_id: true
-# 	validates :parent_id, valid_space_id: true
+	validates :building_id, presence: true, valid_building_id: true
+	validates :space_id, valid_space_id: true
 
   belongs_to :building
   belongs_to :space

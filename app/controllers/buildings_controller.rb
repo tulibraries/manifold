8582e29d1d54b9f76c5edc96109c479c6415bf2d
Steven Ng <steven.ng@temple.edu>
2018-05-23 09:28:44 -0400

    Remove unused views and add controller specs

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -12,55 +12,6 @@ class BuildingsController < ApplicationController
   def show
   end
 
-  # GET /buildings/new
-  def new
-    @building = Building.new
-  end
-
-  # GET /buildings/1/edit
-  def edit
-  end
-
-  # POST /buildings
-  # POST /buildings.json
-  def create
-    @building = Building.new(building_params)
-
-    respond_to do |format|
-      if @building.save
-        format.html { redirect_to @building, notice: 'Building was successfully created.' }
-        format.json { render :show, status: :created, location: @building }
-      else
-        format.html { render :new }
-        format.json { render json: @building.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /buildings/1
-  # PATCH/PUT /buildings/1.json
-  def update
-    respond_to do |format|
-      if @building.update(building_params)
-        format.html { redirect_to @building, notice: 'Building was successfully updated.' }
-        format.json { render :show, status: :ok, location: @building }
-      else
-        format.html { render :edit }
-        format.json { render json: @building.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /buildings/1
-  # DELETE /buildings/1.json
-  def destroy
-    @building.destroy
-    respond_to do |format|
-      format.html { redirect_to buildings_url, notice: 'Building was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_building
---
+++ spec/controllers/buildings_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe BuildingsController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    let(:building) { FactoryBot.create(:building) }
+
+    it "returns http success" do
+      get :show, params: { id: building.id }
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end

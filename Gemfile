944cc2eb891945507eab939eece53b53367dcabd
Steven Ng <steven.ng@temple.edu>
2018-05-31 16:56:41 -0400

    Configure for Google-OmniAuth2

--- Gemfile
+++ Gemfile
@@ -38,7 +38,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'devise'
 gem 'omniauth'
-gem 'omniauth-google'
+gem 'omniauth-google-oauth2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,10 +1,12 @@
 class Account < ApplicationRecord
   include Validators
 
+  has_and_belongs_to_many :oauth_credentials
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :trackable, 
-         :timeoutable, :omniauthable
-
+  devise :trackable, :timeoutable, :omniauthable,
+         :database_authenticatable, :registerable,   # TODO: Remove after OAuth implemented
+         :recoverable, :rememberable, :validatable   # TODO: Remove after OAuth implemented
  	validates :email, tu_access_email: true
 end

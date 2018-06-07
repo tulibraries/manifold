dd37843893b055ad159a55245d2fe45381ef199a
Steven Ng <steven@nomadicoder.com>
2018-06-07 10:32:34 -0400

    Remove HMBT from person and group

---
+++ db/migrate/20180607140824_remove_person_from_groups.rb
@@ -0,0 +1,5 @@
+class RemovePersonFromGroups < ActiveRecord::Migration[5.2]
+  def change
+    remove_reference :groups, :person, foreign_key: true
+  end
+end
---
+++ db/migrate/20180607140942_remove_group_from_person.rb
@@ -0,0 +1,5 @@
+class RemoveGroupFromPerson < ActiveRecord::Migration[5.2]
+  def change
+    remove_reference :people, :group, foreign_key: true
+  end
+end

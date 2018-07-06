d646aeed865e9ec035ad3235b33a32c287d55a80
nomadicoder <steven@nomadicoder.com>
2018-07-06 13:11:10 -0400

    Validates presence of building
    - Reinstates validation of presence of building.
    - No longer tests for valid building
    - Refactor FactoryBot for spaces

--- app/models/space.rb
+++ app/models/space.rb
@@ -5,6 +5,7 @@ class Space < ApplicationRecord
   validates :name, :description, :hours, :accessibility, :location, :image, presence: true
  	validates :email, presence: true, email: true
  	validates :phone_number, presence: true, phone_number: true
+  validates :building_id, presence: true
 
   belongs_to :building
 

9cf17784af83b175bc84bab8b9b355c6b0e423ee
Chris Doyle <cdoyle@temple.edu>
2018-07-06 12:14:27 -0400

    removed oauth field from index.

--- app/dashboards/account_dashboard.rb
+++ app/dashboards/account_dashboard.rb
@@ -31,7 +31,6 @@ class AccountDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :oauth_credentials,
     :id,
     :email,
   ].freeze

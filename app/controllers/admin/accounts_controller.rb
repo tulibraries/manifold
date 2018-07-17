b65c5d971e451cb33baa04938de6ae66979283a2
nomadicoder <steven@nomadicoder.com>
2018-07-17 10:33:53 -0400

    Comment explaining need for current_user method

--- app/controllers/admin/accounts_controller.rb
+++ app/controllers/admin/accounts_controller.rb
@@ -2,6 +2,10 @@ module Admin
   class AccountsController < Admin::ApplicationController
     load_and_authorize_resource
 
+    # Devise has current_user hard_coded so if we us anything other than
+    # user, we have no access to the devise object. So, we need to override
+    # current_user to return the current account. This is needed both
+    # in ApplicationController and in Admin::ApplicationController
     def current_user
       current_account
     end

d93854b9023e83156dd2bf824380fb2ac54ad6da
nomadicoder <steven@nomadicoder.com>
2018-08-06 16:04:35 -0400

    Handle chair_dept_head when creating group

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -15,6 +15,24 @@ module Admin
     #   Group.find_by!(slug: param)
     # end
 
+    def create
+      group_params = params[:group]
+      group_params.permit!
+      group_params[:chair_dept_head] = Person.find_by_id(group_params[:chair_dept_head].to_i)
+      group = Group.new(group_params)
+
+      if group.save
+        redirect_to(
+          [namespace, group],
+          notice: translate_with_resource("create.success"),
+        )
+      else
+        render :new, locals: {
+          page: Administrate::Page::Form.new(dashboard, group),
+        }
+      end
+    end
+
     def update
       group = Group.find(params[:id])
       group_params = params[:group]

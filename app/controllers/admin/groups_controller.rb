ace74a62cb9e18f8143c3ee77f504bffed3baa9e
nomadicoder <steven@nomadicoder.com>
2018-08-06 12:08:03 -0400

    Replace chair_dept_head with person object
    - Default condition from the selector way of doing forms has the 
    chair_dept_head as an ID which administrate passes to the controller 
    update. This should be a person object. This takes the ID and replaces 
    it with the person object for the ID.

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -15,6 +15,23 @@ module Admin
     #   Group.find_by!(slug: param)
     # end
 
+    def update
+      group = Group.find(params[:id])
+      group_params = params[:group]
+      group_params.permit!
+      group_params[:chair_dept_head] = Person.find(group_params[:chair_dept_head].to_i)
+      if group.update(group_params)
+        redirect_to(
+          [namespace, group],
+          notice: translate_with_resource("update.success"),
+        )
+      else
+        render :edit, locals: {
+          page: Administrate::Page::Form.new(dashboard, group),
+        }
+      end
+    end
+
     # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
     # for more information
   end

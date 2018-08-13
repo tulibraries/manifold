f1950b45189299cb1c759e9bba48306f3e673ef2
nomadicoder <steven@nomadicoder.com>
2018-08-13 15:32:53 -0400

    Remove obsolete image field spec

--- spec/factories/buildings.rb
+++ spec/factories/buildings.rb
@@ -7,7 +7,6 @@ FactoryBot.define do
     directions_map "http://maps.exammple.edu/CSA.jpg"
     hours "Always Open"
     phone_number "2155551212"
-    image "https://i.pinimg.com/736x/c8/51/2f/c8512fd8b92570627bbd35f44171d44d.jpg"
     campus "Main Campus"
     email "csa@example.edu"
 

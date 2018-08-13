758aa0f904e293183d5a748bae3c62eb65da489b
nomadicoder <steven@nomadicoder.com>
2018-08-13 16:48:51 -0400

    Fix building specs and validation
    - Remove image validation. This is optional and implemented with 
    active_storage.
    - Validate email format.
    - Don't need to autostrip phone number because we clean it up when we 
    normalize.
    - Use i18n.t translation to verify error messages
    - Remove TDD tests. Not needed once the code's been written.

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,11 +2,11 @@ class Building < ApplicationRecord
   include Validators
   include InputCleaner
 
-  validates :name, :description, :address1, :temple_building_code, :directions_map, :hours, :image, :campus, presence: true
-	#validates :email, presence: true, email: true
+  validates :name, :description, :address1, :temple_building_code, :directions_map, :hours, :campus, presence: true
+	validates :email, presence: true, email: true
 	validates :phone_number, presence: true, phone_number: true
 
-  auto_strip_attributes :email, :phone_number
+  auto_strip_attributes :email
 
   before_validation :normalize_phone_number
 end

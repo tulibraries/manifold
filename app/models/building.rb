6ff194a031f94c6ef551986e3910f21b20d8b190
Steven Ng <steven@nomadicoder.com>
2018-08-14 11:16:15 -0400

    Clean up formatting

--- app/models/building.rb
+++ app/models/building.rb
@@ -3,8 +3,8 @@ class Building < ApplicationRecord
   include InputCleaner
 
   validates :name, :description, :address1, :temple_building_code, :directions_map, :hours, :campus, presence: true
-	validates :email, presence: true, email: true
-	validates :phone_number, presence: true, phone_number: true
+  validates :email, presence: true, email: true
+  validates :phone_number, presence: true, phone_number: true
 
   auto_strip_attributes :email
 

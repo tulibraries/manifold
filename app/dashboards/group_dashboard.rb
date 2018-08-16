d0b4404e5bd5f9d4ef86acd9983dd57c1149fd2d
Chris Doyle <cdoyle@temple.edu>
2018-08-16 13:00:34 -0400

    Move group select options into config.

--- app/dashboards/group_dashboard.rb
+++ app/dashboards/group_dashboard.rb
@@ -21,7 +21,7 @@ class GroupDashboard < Administrate::BaseDashboard
     document: DocumentField,
     external: Field::Boolean,
     group_type: Field::Select.with_options(
-      collection: ["Assembly","Committee","Department","Functional Team","Strategic Steering Team","Working Group"]
+      collection: Rails.configuration.select_options_for_group
       ),
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module Tude
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.select_options_for_group = ["Assembly","Committee","Department","Functional Team","Strategic Steering Team","Working Group"]
   end
 end

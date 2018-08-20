4a378e93e8d75cc153f049c4b30346ff9806d8e1
nomadicoder <steven@nomadicoder.com>
2018-08-20 15:30:46 -0400

    Implement remaining request specs

---
+++ spec/requests/people_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "People", type: :request do
+  let(:building) { FactoryBot.create(:building) }
+  let(:space) { FactoryBot.create(:space, building_id: building.id) }
+  describe "GET /people" do
+    it "renders the person home page with all the people" do
+      person = FactoryBot.create(:person, space_ids: [space.id])
+      get people_path
+      expect(response).to render_template(:index)
+      expect(response.body).to include(person.name)
+    end
+
+    it "works! (now write some real specs)" do
+      person = FactoryBot.create(:person, space_ids: [space.id])
+      get people_path + "/#{person.id}"
+      expect(response).to render_template(:show)
+      expect(response.body).to include(person.name)
+    end
+  end
+end

ac6762d2eac66d04bdae60e5dc65e050558370ba
Steven Ng <steven@nomadicoder.com>
2018-09-04 14:02:09 -0400

    Get FQDN from request environment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def google_oauth_redirect_uri
-    ENV.fetch('GOOGLE_OAUTH_REDIRECT_URI') { 'http://localhost:3000/accounts/auth/google_oauth2' }
+    return "#{request.env['rack.url_scheme']}://#{request.env["HTTP_HOST"]}/accounts/auth/google_oauth2"
   end
 end

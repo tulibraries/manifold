1d0d3e0664a9ef8fea273158eb23eb8994cd0ee0
Steven Ng <steven@nomadicoder.com>
2018-09-07 12:06:26 -0400

    Re-enable description validation (#86)

--- app/models/building.rb
+++ app/models/building.rb
@@ -5,7 +5,7 @@ class Building < ApplicationRecord
   validates :name, :address1, :temple_building_code, :directions_map, :hours, :campus, presence: true
 	validates :email, presence: true, email: true
 	validates :phone_number, presence: true, phone_number: true
-  # TODO implement: validates :description, presence: true
+  validates :description, presence: true
 
 	has_one_attached :photo, dependent: :destroy
 

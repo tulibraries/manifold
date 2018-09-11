050096469f69e0bdc46826ec4ee6a5a8e3fbdf7c
Steven Ng <steven@nomadicoder.com>
2018-09-11 15:01:49 -0400

    Associate groups to services and test join models
    - Validates both services to groups and spaces join models

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,9 @@
 class Service < ApplicationRecord
   validates :title, :description, :intended_audience, :service_category, presence: true
+
+  has_many :service_space
+  has_many :related_spaces, through: :service_space, source: :space
+
+  has_many :service_group
+  has_many :related_groups, through: :service_group, source: :group
 end
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -26,4 +26,35 @@ RSpec.describe Service, type: :model do
       expect { service.save! }.to raise_error(/Service category can't be blank/)
     end
   end
+
+  describe "associated class" do
+    let(:building) { FactoryBot.create(:building) }
+    let(:space) { FactoryBot.create(:space, building: building) }
+    context "Space" do
+      example "attach space" do
+        service = FactoryBot.create(:service, related_spaces: [space])
+        expect(service.related_spaces.first.name).to match(/#{Space.first.name}/)
+      end
+      example "no space" do
+        service = FactoryBot.create(:service)
+        expect { service.save! }.to_not raise_error
+      end
+    end
+    context "Group" do
+      let(:person) { FactoryBot.build(:person, spaces: [space]) }
+      let(:group) { FactoryBot.create(:group, persons: [person], spaces: [space], chair_dept_head: person) }
+      example "attach group" do
+        service = FactoryBot.create(:service, related_groups: [group])
+        expect(service.related_groups.first.name).to match(/#{Group.first.name}/)
+      end
+      example "not present" do
+        skip "not implemented"
+        service = FactoryBot.create(:service)
+        expect { service.save! }.to raise_error
+      end
+    end
+    context "Policy" do
+      skip "TBD"
+    end
+  end
 end

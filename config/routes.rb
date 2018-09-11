f78d3ef66b8948d3536848055e226aa2363455eb
Chris Doyle <cdoyle@temple.edu>
2018-09-11 11:57:03 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,4 @@ Rails.application.routes.draw do
     get 'hours' => :index
     get 'hours/:id' => :show
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

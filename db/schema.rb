b71094d6895e50c17bc07f1dbf028554be4076f1
Chad Nelson <chadbnelson@gmail.com>
2018-09-18 15:16:52 -0400

    Make sure schema matches migrations (#95)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_09_13_154916) do
 
   create_table "accounts", force: :cascade do |t|

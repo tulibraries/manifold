6204fd5c99699863fe3c6392fad6332e0abb1fcf
Chad Nelson <chad.nelson@temple.edu>
2018-10-16 15:25:36 -0400

    Remove factory deprecations

--- spec/factories/services.rb
+++ spec/factories/services.rb
@@ -3,11 +3,13 @@
 FactoryBot.define do
   factory :service do
     sequence(:title) { |n| "Service #{n}" }
-    description <<~EOD.strip.gsub(/\n/, " ")
+    description {
+    <<~EOD.strip.gsub(/\n/, " ")
       The best drink in existence is the Pan Galactic Gargle Blaster, the effect
       of which is like having your brains smashed out by a slice of lemon wrapped
       round a large gold brick.
     EOD
+  }
     access_description { "Fully accessible" }
     access_link { |n| "http://www.example.com/#{n}" }
     service_policies { "Plenary" }

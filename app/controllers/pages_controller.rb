4694439da184c2b5f1172661031d555bc2e8de64
Chris Doyle <cdoyle@temple.edu>
2018-10-19 16:19:53 -0400

    Removed unnecessary instance variable from page controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+    before_action :set_date, only: [:about_ambler]
+
 class PagesController < ApplicationController
   def home
   end
@@ -7,4 +9,20 @@ class PagesController < ApplicationController
   def get_highlights
     @highlights = Highlight.where(promoted: true).take(4)
   end
+
+  def ambler
+  end
+
+  def index
+    @buildings = Building.all
+  end
+
+  def show
+    @todays_hours = LibraryHours.where(location_id: @building.hours, date: @today)
+  end
+
+  private
+    def set_date
+      @today = Date.today.strftime("%Y-%m-%d 00:00:00")
+    end
 end

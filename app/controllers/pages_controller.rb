362a582fcf0782ec7351f3cedabc062b5c5d3457
Chris Doyle <cdoyle@temple.edu>
2018-10-22 15:51:00 -0400

    Cleaned up pages controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,28 +1,12 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  before_action :set_date, only: [:about_ambler]
-  
-  def home
-  end
 
   def get_highlights
     @highlights = Highlight.where(promoted: true).take(4)
   end
 
-  def ambler
-  end
-
   def index
     @buildings = Building.all
   end
-
-  def show
-    @todays_hours = LibraryHours.where(location_id: @building.hours, date: @today)
-  end
-
-  private
-    def set_date
-      @today = Date.today.strftime("%Y-%m-%d 00:00:00")
-    end
 end

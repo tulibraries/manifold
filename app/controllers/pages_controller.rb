21e5bef72074160d14cce9afc72fb88e006d2141
Chris Doyle <cdoyle@temple.edu>
2018-10-25 13:22:28 -0400

    Man 70 ambler pages (#128)
    * Removed search options on secondary pages, added action to bento search.
    
    * Refactored searchbar, eliminating the partial amd combining with header partial. Page titles no longer being in the green bar at top there's no need for customizing the green bar per page.
    
    * Removed green bar from page level and added search and breadcrumbs.
    
    * removed test line that didn't work for intention.
    
    * Remove inline styles.
    
    * Added Ambler homepage.
    
    * Linnked about page to building path.
    
    * Removed unnecessary instance variable from page controller.
    
    * Corrected controller syntax.
    
    * Cleaned up pages controller.
    
    * Putting back home and mabler functions.
    
    * Removed unneeded index methos from pages controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  def home
-  end
 
   def get_highlights
     @highlights = Highlight.where(promoted: true).take(4)
+  end
+
+  def home
+  end
+
+  def ambler
   end
 end

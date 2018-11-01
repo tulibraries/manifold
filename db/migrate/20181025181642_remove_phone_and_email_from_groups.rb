901b8596601962dbab97c92d759a26a66689f407
nomadicoder <steven@nomadicoder.com>
2018-11-01 15:43:16 -0400

    Restore phone and email fields in groups
    - Removing these fields caused deployment to fail. Restoring
      just the fields. Leaving the back-end code in place.

--- db/migrate/20181025181642_remove_phone_and_email_from_groups.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RemovePhoneAndEmailFromGroups < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :groups, :phone_number, :string
-    remove_column :groups, :email_address, :string
-  end
-end

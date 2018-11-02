4598aa64606a4d7261fb745a26d5032e36cbb980
Chris Doyle <cdoyle@temple.edu>
2018-11-02 10:21:31 -0400

    Update pages_controller.rb
    Rubocop did not fix the indentation locally although it passed the local tests.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-	before_action :set_date, :todays_date, only: [:home, :hsl, :ambler]
+  before_action :set_date, :todays_date, only: [:home, :hsl, :ambler]
 
   def get_highlights
     @highlights = Highlight.where(promoted: true).take(4)
@@ -14,16 +14,16 @@ class PagesController < ApplicationController
   end
 
   def hsl
-  	@departments = Group.where(group_type: "Department")
-  	@ginsburg_hours = LibraryHours.where(location_id: "ginsburg", date: @today).pluck(:hours).first
-  	@podiatry_hours = LibraryHours.where(location_id: "podiatry", date: @today).pluck(:hours).first
+    @departments = Group.where(group_type: "Department")
+    @ginsburg_hours = LibraryHours.where(location_id: "ginsburg", date: @today).pluck(:hours).first
+    @podiatry_hours = LibraryHours.where(location_id: "podiatry", date: @today).pluck(:hours).first
   end
 
   private
     def set_date
-    	@today = Date.today.strftime("%Y-%m-%d 00:00:00")
+      @today = Date.today.strftime("%Y-%m-%d 00:00:00")
     end
     def todays_date
-    	@todays_date = @today.to_date.strftime("%^A, %^B %d, %Y ")
+      @todays_date = @today.to_date.strftime("%^A, %^B %d, %Y ")
     end
 end

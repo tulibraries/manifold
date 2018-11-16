bf7bd46a01344f7b6d65d531ada8252da27b174e
Chris Doyle <cdoyle@temple.edu>
2018-11-16 12:34:39 -0500

    added a service belongs to relation in model, and removed validation in tests for location field (unused)

--- app/models/library_hours.rb
+++ app/models/library_hours.rb
@@ -5,5 +5,5 @@ class LibraryHours < ApplicationRecord
 
   belongs_to :building, optional: true
   belongs_to :space, optional: true
-  # belongs_to :service, optional: true
+  belongs_to :service, optional: true
 end

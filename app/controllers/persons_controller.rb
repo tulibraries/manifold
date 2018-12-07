684c1cc7c4d8e40c3238d9a73ecb52525e2180f2
Chris Doyle <cdoyle@temple.edu>
2018-12-07 14:53:51 -0500

    - Sorted instance variable of persons by last_name, first_name. - Adjusted view to display last name first. -Fixed to tests to expect last_name and not 'name'. (#207)

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -7,7 +7,7 @@ class PersonsController < ApplicationController
   # GET /persons
   # GET /persons.json
   def index
-    @persons = Person.all
+    @persons = Person.all.order(:last_name, :first_name)
   end
 
   # GET /persons/1

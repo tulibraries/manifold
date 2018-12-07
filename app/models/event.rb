50a2116952984e227d92e2e5c9acd10c5394b084
nomadicoder <steven@nomadicoder.com>
2018-12-07 10:56:45 -0500

    Fixes text display
    - View was displaying raw html
    
    Fix false positive warning in Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
+  include InputCleaner
   belongs_to :building, optional: true
   belongs_to :space, optional: true
   belongs_to :person, optional: true
   has_one_attached :image, dependent: :destroy
+
+  before_validation :sanitize_description
 end

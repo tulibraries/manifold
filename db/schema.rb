2fa84dd0c420df746eae0a5d3266a1e78432c830
Chris Doyle <cdoyle@temple.edu>
2018-12-11 11:23:08 -0500

    Created responsive version of Plan Your Visit section based on pulling separate template based on screen size.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_173332) do
+ActiveRecord::Schema.define(version: 2018_12_05_150746) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false

8ec13a761ee131db287410733f03d0925c305905
Chris Doyle <cdoyle@temple.edu>
2019-01-02 11:05:16 -0500

    Using the mock-up for the homepage, please create a mobile version. You can use an iPhone sample device.
    - Add responsive styling for Highlights section of homepage
    - Add responsive styling for static  homepage blocks
    - Update search options and hours info for responsive design
    - Updates to pass tests (add strip to description cleaner) and fix some alignments.
    - updates to hsl and ambler homepages to not render quite so badly at low res

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,13 +12,14 @@ class PagesController < ApplicationController
   end
 
   def ambler
+    @events = Event.take(4)
   end
 
   def hsl
     @departments = Group.where(group_type: "Department")
     @ginsburg_hours = LibraryHours.where(location_id: "ginsburg", date: @today).pluck(:hours).first
     @podiatry_hours = LibraryHours.where(location_id: "podiatry", date: @today).pluck(:hours).first
-    @events = Event.where("tags LIKE ?", "%HSL%").take(4)
+    @events = Event.take(4)
   end
 
   private

3376b347133255d3727bcfe13afb4c1cee2bd545
Chris Doyle <cdoyle@temple.edu>
2019-01-09 12:58:30 -0500

    Update highlights to go directly to link from highlight, no show or indes page. Also update Ambler, HSL pages to pull only tagged events.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,7 @@ class Ability
     can :read, [Building, Space, Group, Person, Alert, Service, Collection, Policy] # permissions for every account, even if not logged in
     if account.present?  # additional permissions for logged in accounts (they can manage their posts)
       can :read, :all # permissions for every account, even if not logged in
-      can :manage, [Building, Space, Person, Group, Service, Collection], account_id: account.id
+      can :manage, [Building, Space, Person, Group, Service, Collection, Policy], account_id: account.id
       if account.alertability?  # additional permissions for alertors
         can :update, Alert
       end

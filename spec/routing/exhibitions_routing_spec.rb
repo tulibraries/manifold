ee084375a69a594c16b1accf7c22edd0e1fd9469
Chris Doyle <cdoyle@temple.edu>
2019-01-22 09:15:58 -0500

    Update exhibitions_routing_spec.rb

--- spec/routing/exhibitions_routing_spec.rb
+++ spec/routing/exhibitions_routing_spec.rb
@@ -8,33 +8,8 @@ RSpec.describe ExhibitionsController, type: :routing do
       expect(get: "/exhibitions").to route_to("exhibitions#index")
     end
 
-    it "routes to #new" do
-      expect(get: "/exhibitions/new").to route_to("exhibitions#new")
-    end
-
     it "routes to #show" do
       expect(get: "/exhibitions/1").to route_to("exhibitions#show", id: "1")
-    end
-
-    it "routes to #edit" do
-      expect(get: "/exhibitions/1/edit").to route_to("exhibitions#edit", id: "1")
-    end
-
-
-    it "routes to #create" do
-      expect(post: "/exhibitions").to route_to("exhibitions#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(put: "/exhibitions/1").to route_to("exhibitions#update", id: "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(patch: "/exhibitions/1").to route_to("exhibitions#update", id: "1")
-    end
-
-    it "routes to #destroy" do
-      expect(delete: "/exhibitions/1").to route_to("exhibitions#destroy", id: "1")
     end
   end
 end

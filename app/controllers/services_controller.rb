8e6ee9bd7d0cd9905afce3fb2231904cfc1a5dd1
Chris Doyle <cdoyle@temple.edu>
2019-02-05 09:55:02 -0500

    Update services_controller.rb

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,27 +4,15 @@ class ServicesController < ApplicationController
   load_and_authorize_resource
   before_action :set_service, only: [:show]
 
-  # GET /services
-  # GET /services.json
-  def index
-    @services = Service.all
-  end
-
-  # GET /services/1
-  # GET /services/1.json
   def show
     @services = Service.all
-    @related_services = @services.where(service_category: @service.service_category).where.not(id: @service.id)
+    groups = @services.group_by { |service| service.service_category }
+    @grouped_services = Hash[ groups.sort_by { |key, val| key } ]
+    @key_group = @service.service_category
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_service
       @service = Service.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def service_params
-      params.require(:service).permit()
     end
 end

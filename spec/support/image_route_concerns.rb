e71a965437b26b5e6a71ccaad6ab4ead3cceb231
Chad Nelson <chad.nelson@temple.edu>
2019-04-30 09:47:27 -0400

    Add restish route support for images via concern (#401)
    * WIP - image route concern
    
    * Add concern to multiple resources
    
    Add actions for different sizes of images and routes to those.
    
    * Add restish route support for images
    
    Allows thumbnail, medium, and large images for entites with images to be
    accessed via a standarised route. The route redirects to the
    appropriate active_storage route, but we don't have to embed those urls
    into JSON representations of the data.
    
    Adds route and controller teets.
    
    * Use uniqe method for model name

---
+++ spec/support/image_route_concerns.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+
+def model_name_from_controller
+  described_class.to_s.underscore.gsub(/_controller/, "").singularize
+end
+
+RSpec.shared_examples "routes_for_imageable" do
+
+  describe "image routes for #{described_class}", type: :routing do
+
+  let(:factory_model) { FactoryBot.create(model_name_from_controller.to_sym, :with_image) }
+  let(:thumnbnail_path) { send("#{model_name_from_controller}_image_thumbnail_path", factory_model) }
+  let(:medium_path) { send("#{model_name_from_controller}_image_medium_path", factory_model) }
+  let(:large_path) { send("#{model_name_from_controller}_image_large_path", factory_model) }
+
+  context "object has an attached image" do
+    it "has route to a thumnbnail image" do
+      expect(get: thumnbnail_path).to be_routable
+    end
+
+    it "has route to a medium image" do
+      expect(get: medium_path).to be_routable
+    end
+
+    it "has route to a large image" do
+      expect(get: large_path).to be_routable
+    end
+
+
+  end
+
+  context "object does not have an attached image" do
+
+  end
+
+end
+end

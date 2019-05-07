e679fcdf287d2488ad63d5bb5402c03a83f71648
Chris Doyle <cdoyle@temple.edu>
2019-05-07 10:17:53 -0400

    Changed left navigation to pull other entities in same categories as displayed service. Adjusted styles for Desktop view according to ticket. (#412)

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -5,14 +5,15 @@ class ServicesController < ApplicationController
   before_action :set_service, only: [:show]
 
   def show
-    @services = Service.all
-    groups = @services.group_by { |service| service.service_category }
-    @grouped_services = Hash[ groups.sort_by { |key, val| key } ]
-    @key_group = @service.service_category
+    respond_to do |format|
+      format.html
+      # format.json { render json: ServiceSerializer.new(@service) }
+    end
   end
 
   private
     def set_service
       @service = Service.find(params[:id])
+      @categories = @service.categories
     end
 end

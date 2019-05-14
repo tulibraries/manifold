b224c904527cdc6986bf55ade3830d099f23527b
Steven Ng <steven@nomadicoder.com>
2019-05-14 14:56:45 -0400

    Added updated_at to collection JSON response

--- app/serializers/collection_serializer.rb
+++ app/serializers/collection_serializer.rb
@@ -2,5 +2,5 @@
 
 class CollectionSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :description, :subject, :contents, :add_to_footer, :label
+  attributes :name, :description, :subject, :contents, :add_to_footer, :label, :updated_at
 end

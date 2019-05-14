2c6318807db114b8cf9b1d6fcf4caeeb6c6ed5b4
Steven Ng <steven@nomadicoder.com>
2019-05-14 14:41:40 -0400

    MAN-282 External link entity (#427)
    * Add external link entity
    
    Add external_link dashboard
    
    Make categorizable
    
    * Customize dashboard superclass definition

---
+++ app/models/external_link.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class ExternalLink < ApplicationRecord
+  has_paper_trail
+  include Validators
+  include Categorizable
+
+  validates :title, presence: true
+  validates :link, presence: true, url: true
+end

b6ad7038d8e714d6bb153a7916e399a3c485b299
Steven Ng <steven@nomadicoder.com>
2019-05-20 11:36:59 -0400

    Fix thumbnail URL helper

--- app/serializers/concerns/image_serializable.rb
+++ app/serializers/concerns/image_serializable.rb
@@ -9,7 +9,7 @@ module ImageSerializable
     end
 
     attribute :thumbnail_image, if: Proc.new { |the_object| the_object.image.attached? } do |the_object|
-      helpers.send("#{the_object.class.to_s.underscore}_image_large_url", the_object)
+      helpers.send("#{the_object.class.to_s.underscore}_image_thumbnail_url", the_object)
     end
   end
 end

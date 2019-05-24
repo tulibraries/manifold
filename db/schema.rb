8792d8af661d12243d08660f4742dfdcb9bccc20
Steven Ng <steven@nomadicoder.com>
2019-05-24 11:08:27 -0400

    Add external_link to buildings (#457)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_202839) do
+ActiveRecord::Schema.define(version: 2019_05_23_193908) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -103,6 +103,8 @@ ActiveRecord::Schema.define(version: 2019_05_20_202839) do
     t.string "google_id"
     t.string "address2"
     t.boolean "add_to_footer"
+    t.integer "external_link_id"
+    t.index ["external_link_id"], name: "index_buildings_on_external_link_id"
   end
 
   create_table "categories", force: :cascade do |t|

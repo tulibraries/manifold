db453f9d07f93e7e47dd8334c1d8daccd844dd60
Steven Ng <steven@nomadicoder.com>
2019-05-28 10:55:18 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_05_23_193908) do
-=======
 ActiveRecord::Schema.define(version: 2019_05_24_142559) do
->>>>>>> Create description attribute
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false

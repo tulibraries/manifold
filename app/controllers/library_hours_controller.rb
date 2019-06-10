ea93573eb141aae522347d87993a0b513ef9cf91
Chris Doyle <cdoyle@temple.edu>
2019-06-10 11:27:21 -0400

    #Man-346 hours display for buildings and spaces (#463)
    * List entities in category admin templates (#456)
    
    * Move configration parameters to application config

--- app/controllers/library_hours_controller.rb
+++ app/controllers/library_hours_controller.rb
@@ -1,44 +1,12 @@
 # frozen_string_literal: true
 
 class LibraryHoursController < ApplicationController
-  before_action :get_locations, :set_dates, only: [:index]
+  before_action :buildings, :set_dates, only: [:index, :show]
 
   def index
-    @buildings = [
-      {
-        slug: "charles",
-        spaces: [
-                  "charles",
-                  "service_zone",
-                  "cafe",
-                  "scrc",
-                  "scholars_studio",
-                  "success_center",
-                  "ask_a_librarian",
-                  "asrs",
-                  "guest_computers"
-                ]
-      },
-      {
-        slug: "ginsburg",
-        spaces: ["ginsburg", "innovation"]
-      },
-      {
-        slug: "podiatry",
-        spaces: ["podiatry"]
-      },
-      {
-        slug: "ambler",
-        spaces: ["ambler"]
-      },
-      {
-        slug: "blockson",
-        spaces: ["blockson"]
-      }
-    ]
     @buildings.each do |building|
-      building.values.second.map! do |space|
-        space = [building.values.first, LibraryHour.where(location_id: space, date: @monday..@sunday + 1)]
+      building[:spaces].map! do |space|
+        space = [building[:slug], LibraryHour.where(location_id: space, date: @monday..@sunday + 1)]
       end
     end
   end
@@ -60,18 +28,8 @@ class LibraryHoursController < ApplicationController
     end
   end
 
-  def set_location
-    @location = Building.where(hours: params[:id])
-    if @location.blank?
-      @location = Space.where(hours: params[:id])
-    end
-    if @location.blank?
-      @location = Service.where(hours: params[:id])
-    end
-  end
-
-  def get_locations
-    @location = LibraryHour.distinct.pluck(:location_id)
+  def buildings
+    @buildings = Rails.configuration.building_spaces
   end
 
   def build_hours_data_structure(input)

fe4891f1a37c278c0305d35ab524f608ead865ed
Steven Ng <steven@nomadicoder.com>
2019-06-11 13:40:49 -0400

    Add external link to space model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_142559) do
+ActiveRecord::Schema.define(version: 2019_06_11_155116) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -397,8 +397,10 @@ ActiveRecord::Schema.define(version: 2019_05_24_142559) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ancestry"
+    t.integer "external_link_id"
     t.index ["ancestry"], name: "index_spaces_on_ancestry"
     t.index ["building_id"], name: "index_spaces_on_building_id"
+    t.index ["external_link_id"], name: "index_spaces_on_external_link_id"
   end
 
   create_table "versions", force: :cascade do |t|

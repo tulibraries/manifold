641128aee1083fb04de35ce66386c6c899a58d31
Chris Doyle <cdoyle@temple.edu>
2019-06-13 16:33:18 -0400

    created partials for each filter. Started refactorin index view. Complete styling for index view excpet for filters. Add pagination per 5 Create helper for subjects array Cleaning up filter lists

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -10,6 +10,13 @@ class PersonsController < ApplicationController
     else
       @persons = Person.where("last_name LIKE ?", "#{params[:id]}%").order(:last_name, :first_name)
     end
+
+    @persons_list = @persons.page params[:page]
+    @specialists = @persons.where.not(specialties: nil)
+    @subjects = (@persons.where.not(specialties: nil).collect { |p| p.specialties }.flatten.uniq.sort).reject!{|s| s.empty?}
+    @departments = (@persons.where.not(groups: nil).collect { |p| p.groups }.flatten.uniq.sort)
+    @locations = (@persons.where.not(spaces: nil).collect { |p| p.spaces }.flatten.uniq.sort)
+    
     respond_to do |format|
       format.html
       format.json { render json: PersonSerializer.new(@persons.to_a) }
--- app/helpers/persons_helper.rb
+++ app/helpers/persons_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 module PersonsHelper
+
+	def depts_list(person)
+		depts = Person.find(person.id).groups.select { |group| group.group_type == "Department" }
+		depts.collect(&:label).join(",<br />")
+	end
+
+	def specialties(person)
+		person.specialties.drop(1).each do |specialty|
+      specialty
+    end
+  end
+	
 end

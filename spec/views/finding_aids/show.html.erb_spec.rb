e75dd8f9788d0404710ab4725963376b66955991
Chris Doyle <cdoyle@temple.edu>
2019-07-03 16:59:41 -0400

    Updated display of finding aid show page. Update link on collections page to go to finding aids index with current collection preselected.

--- spec/views/finding_aids/show.html.erb_spec.rb
+++ spec/views/finding_aids/show.html.erb_spec.rb
@@ -3,24 +3,14 @@
 require "rails_helper"
 
 RSpec.describe "finding_aids/show", type: :view do
-  before(:each) do
+  before(:all) do
     @collection = FactoryBot.create(:collection)
-    @finding_aid = assign(:finding_aid, FindingAid.create!(
-                                          name: "Name",
-                                          description: "MyText",
-                                          subject: ["Subject"],
-                                          content_link: "Content Link",
-                                          identifier: "Identifier",
-      # collection: @collection
-    ))
   end
-
-  it "renders attributes in <p>" do
+  it "renders attributes" do
+    @finding_aid = FactoryBot.create(:finding_aid, collections: [@collection])
     render
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/Description/)
     expect(rendered).to match(/Subject/)
-    expect(rendered).to match(/Content Link/)
     expect(rendered).to match(/Identifier/)
   end
 end

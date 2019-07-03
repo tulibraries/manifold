607c580e90e9e7ffd94a48bf5207c7873e7a3299
Chris Doyle <cdoyle@temple.edu>
2019-07-03 16:27:50 -0400

    Create finding aid index page with filters (#524)
    Fine-tune filters
    Add intro text with links to mini homepages yet to be created
    Added paths for above minipages to routes but no templaes yet exist for them
    Updates for responsive design
    Added scrc and blockson pages so links form this page don't fail

--- app/controllers/finding_aids_controller.rb
+++ app/controllers/finding_aids_controller.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class FindingAidsController < ApplicationController
+  include FindingAidFilters
+
   before_action :set_finding_aid, only: [:show]
+  before_action :return_aids, only: [:index]
 
   def index
     @finding_aids = FindingAid.all
+
     respond_to do |format|
       format.html
       format.json { render json: FindingAidSerializer.new(@finding_aids) }
@@ -16,6 +20,30 @@ class FindingAidsController < ApplicationController
       format.html
       format.json { render json: FindingAidSerializer.new(@finding_aid) }
     end
+  end
+
+  def return_aids
+    all_aids = FindingAid.group(:id).order(:title)
+
+    if params.has_key?("collection")
+      collections = collections_list(all_aids)
+    end
+    if params.has_key?("subject")
+      subjects = subjects_list(all_aids)
+    end
+
+    arrays = [Array(collections), Array(subjects)].reject(&:empty?).reduce(:&) || []
+
+    filtered_aids = arrays.blank? ? all_aids : arrays
+
+    get_filters(filtered_aids)
+    aids = FindingAid.where(id: filtered_aids.map(&:id)).order(:title)
+    @aids_list = aids.page params[:page]
+  end
+
+  def get_filters(aids)
+    @subjects = aids.select { |aid| aid.subject.try(:any?) }.collect { |s| s.subject }.flatten.uniq.sort
+    @collections = aids.select { |aid| aid.collections.try(:any?) }.collect { |c| c.collections }.flatten.uniq.sort
   end
 
   private

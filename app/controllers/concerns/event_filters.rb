55befe5cb02f03166d15304281facffb74e7108d
Chris Doyle <cdoyle@temple.edu>
2019-07-08 21:36:52 -0400

    Refactor facets.

---
+++ app/controllers/concerns/event_filters.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module EventFilters
+  extend ActiveSupport::Concern
+
+  def types_list(events)
+    to_add = events.select { |event| event.event_type { |type| type == params[:type] } }
+
+    types = to_add.map { |event| event.event_type }
+
+    types.uniq.sort
+  end
+
+  def locations_list(events)
+    to_add = events.select { |event| event.building == params[:location] || event.external_building == params[:location] }
+
+    locations = to_add.map { |event| 
+      unless event.building.nil?
+        event.building.label
+      else
+        event.external_building
+      end }
+
+    locations.uniq.sort
+  end
+end

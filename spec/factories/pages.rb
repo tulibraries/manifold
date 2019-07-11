3ca58a28a6ddefcd2293c9d0e3670e4a74452d65
Steven Ng <steven@nomadicoder.com>
2019-07-11 14:34:59 -0400

    Request specs

--- spec/factories/pages.rb
+++ spec/factories/pages.rb
@@ -5,5 +5,12 @@ FactoryBot.define do
     title { "MyString" }
     description { "MyText" }
     layout { "MyLayout" }
+    trait :with_document do
+      after :create do |page|
+        file_path = Rails.root.join("spec", "support", "assets", "hal.png")
+        file = fixture_file_upload(file_path, "image/png")
+        page.document.attach(file)
+      end
+    end
   end
 end
--- spec/requests/admin/person_spec.rb
+++ spec/requests/admin/person_spec.rb
@@ -3,5 +3,20 @@
 require "rails_helper"
 
 RSpec.describe Person, type: :request do
-  it_behaves_like "renderable_dashboard"
+  describe "GET /admin/people/:id/detach" do
+    it "detaches image from people" do
+      person = FactoryBot.create(:person, :with_image)
+      login_as(FactoryBot.create(:administrator), scope: :account)
+
+      get "/admin/people/#{person.id}"
+      expect(response).to render_template(:show)
+      expect(response.body).to include("hal.png")
+
+      get "/admin/people/#{person.id}/detach"
+      follow_redirect!
+
+      expect(response).to render_template(:show)
+      expect(response.body).to_not include("hal.png")
+    end
+  end
 end

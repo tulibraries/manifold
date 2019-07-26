a75254e1079af7fd115d98982fecf9989a628e2a
Chris Doyle <cdoyle@temple.edu>
2019-07-26 11:50:43 -0400

    Added chat widget button via JS to all pages in main menu. Update jumble.js to add chat widget to mobile menu. (#625)

--- app/assets/javascripts/jumble.js
+++ app/assets/javascripts/jumble.js
@@ -2,10 +2,13 @@ $(document).ready(function() {
   if ($(window).width() < 1040) {
     $('#scrc-left').insertAfter('#scrc-description');
     $('#dsc-left').insertAfter('#dsc-description');
+    $('#libchat_fe2bd0cb1f04720f51641c4f01b8e22e').appendTo('#chat-nav-mobile');
+
   }
   if($(window).width() > 1040) {
     $('#scrc-left').insertBefore('#scrc-description-container');
     $('#dsc-left').insertBefore('#dsc-description-container');
+    $('#libchat_fe2bd0cb1f04720f51641c4f01b8e22e').appendTo('#chat-nav');
   }
 });
 
@@ -13,9 +16,11 @@ $(window).on('resize', function() {
   if ($(window).width() < 1040) {
     $('#scrc-left').insertAfter('#scrc-description');
     $('#dsc-left').insertAfter('#dsc-description');
+    $('#libchat_fe2bd0cb1f04720f51641c4f01b8e22e').appendTo('#chat-nav-mobile');
   }
   if($(window).width() > 1040) {
     $('#scrc-left').insertBefore('#scrc-description-container');
     $('#dsc-left').insertBefore('#dsc-description-container');
+    $('#libchat_fe2bd0cb1f04720f51641c4f01b8e22e').appendTo('#chat-nav');
   }
 });

0555f4ef2ab6179a3e985213c3137d6807094dcb
Chris Doyle <cdoyle@temple.edu>
2019-08-28 14:37:45 -0400

    Added personal statement as field rather than relying on comments field.

--- spec/requests/forms/cac_external_spec.rb
+++ spec/requests/forms/cac_external_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "Cultural Analytics Certificate External Application", type: :req
     {
       name: "Ringo", email: "tu@temple.edu", phone: "2152041234", bachelor_degree: "Yes",
       institution_of_degree: "Temple", overall_gpa: "4.0", degrees_earned: "None",
-      comments: "Personal Statement"
+      personal_statement: "Personal Statement"
     }
   }
 

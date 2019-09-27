7cea0f32244b299884d30b3ff90eafd9d4c13972
Steven Ng <steven@nomadicoder.com>
2019-09-27 15:04:55 -0400

    Test for subject area and subject filtering

--- spec/features/finding_aids_spec.rb
+++ spec/features/finding_aids_spec.rb
@@ -15,7 +15,8 @@ RSpec.feature "FindingAids", type: :feature do
     before(:all) do
       @collection = FactoryBot.create(:collection)
       @other_collection = FactoryBot.create(:collection, name: "Other Collection")
-      @finding_aid = FactoryBot.create(:finding_aid, collections: [@collection, @other_collection])
+      @finding_aid = FactoryBot.create(:finding_aid, name:"Finding Aid 1", collections: [@collection])
+      @other_finding_aid = FactoryBot.create(:finding_aid, name:"Finding Aid 2", collections: [@other_collection])
     end
 
     after(:all) do
@@ -31,6 +32,8 @@ RSpec.feature "FindingAids", type: :feature do
           expect(page).to have_content(@other_collection.name)
         end
       end
+      expect(page).to have_content(@finding_aid.name)
+      expect(page).to have_content(@other_finding_aid.name)
     end
 
     scenario "Filter by Collection" do
@@ -42,6 +45,45 @@ RSpec.feature "FindingAids", type: :feature do
           expect(page).to_not have_content(@other_collection.name)
         end
       end
+      expect(page).to have_content(@finding_aid.name)
+      expect(page).to_not have_content(@other_finding_aid.name)
+    end
+  end
+
+  context "Subject Area" do
+    Capybara.ignore_hidden_elements = false
+    before(:all) do
+      @finding_aid = FactoryBot.create(:finding_aid, name: "Finding Aid 1")
+      @other_finding_aid = FactoryBot.create(:finding_aid, name: "Finding Aid 2", subject: ["other_subject"])
+    end
+
+    after(:all) do
+      FindingAid.delete_all
+    end
+
+    scenario "Visit finding aid that has an subject" do
+      visit("/finding_aids")
+      within(".aid-index") do
+        within(".filter_subjects") do
+          expect(page).to have_content(@finding_aid.subject.first)
+          expect(page).to have_content(@other_finding_aid.subject.first)
+        end
+      end
+      expect(page).to have_content(@finding_aid.name)
+      expect(page).to have_content(@other_finding_aid.name)
+    end
+
+    scenario "Filter by Subject" do
+      visit("/finding_aids")
+      within(".aid-index") do
+        within(".filter_subjects") do
+          click_on @finding_aid.subject.first
+          expect(page).to have_content(@finding_aid.subject.first)
+          expect(page).to_not have_content(@other_finding_aid.subject.first)
+        end
+      end
+      expect(page).to have_content(@finding_aid.name)
+      expect(page).to_not have_content(@other_finding_aid.name)
     end
   end
 end

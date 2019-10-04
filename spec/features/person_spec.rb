80b80f2b01909daa446e823f4e125663538dc0aa
Steven Ng <steven@nomadicoder.com>
2019-10-04 13:11:07 -0400

    Filter specialists

--- spec/features/person_spec.rb
+++ spec/features/person_spec.rb
@@ -87,4 +87,31 @@ RSpec.feature "People", type: :feature do
     end
   end
 
+  describe "Specialist" do
+    before(:all) do
+      @person1 = FactoryBot.create(:person)
+      @person2 = FactoryBot.create(:person, specialties: [])
+      @person3 = FactoryBot.create(:person, specialties: nil)
+    end
+
+    after(:all) do
+      Person.delete_all
+    end
+
+    scenario "Filter specialists" do
+      visit("/people")
+      within(".staff-index") do
+        expect(page).to have_content(@person1.email_address)
+        expect(page).to have_content(@person2.email_address)
+        expect(page).to have_content(@person3.email_address)
+        within("#staff-type") do
+          click_on("Limit to Subject Librarians Only")
+        end
+        expect(page).to have_content(@person1.email_address)
+        expect(page).to_not have_content(@person2.email_address)
+        expect(page).to_not have_content(@person3.email_address)
+      end
+    end
+  end
+
 end

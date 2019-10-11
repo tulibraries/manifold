c5eea32090ddf1fd8301b9320f7d211aff0284b7
Chad Nelson <chadbnelson@gmail.com>
2019-10-11 11:53:08 -0400

    Revert "#MAN-700 Recreate WPVI Videos page"

--- spec/support/auth_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module AuthHelper
-  def http_login
-    user = "username"
-    pw = "password"
-    request.env["HTTP_AUTHORIZATION"] = ActionController::HttpAuthentication::Basic.encode_credentials(user, pw)
-  end
-end

3d36a475e1a76cd3f076fd69e5e6896548e31144
Chris Doyle <cdoyle@temple.edu>
2019-10-16 14:30:21 -0400

    Update buildings_controller.rb

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -10,7 +10,7 @@ class BuildingsController < ApplicationController
   def index
     @buildings = Building.all
     respond_to do |format|
-      format.html 
+      format.html
       format.json { render json: BuildingSerializer.new(@buildings) }
     end
   end

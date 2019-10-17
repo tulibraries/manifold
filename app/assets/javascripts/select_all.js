1bb32e67c6dc6d616d5d52fa097a76c32be0acf9
Chris Doyle <cdoyle@temple.edu>
2019-10-17 12:34:50 -0400

    Refactor JS calls for mobile menu button icons.

--- app/assets/javascripts/select_all.js
+++ app/assets/javascripts/select_all.js
@@ -1,7 +1,5 @@
-  $(function () {
-      // console.log("Button loaded");
-      $('#select_all').click(function() {
-          // console.log("Button clicked");
-          $('#service_intended_audience option').prop('selected', true);
-      });
+$(function () {
+  $('#select_all').click(function() {
+    $('#service_intended_audience option').prop('selected', true);
   });
+});

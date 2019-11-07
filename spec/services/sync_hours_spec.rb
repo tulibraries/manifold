e738c38111d06d5d6903e95d8cad6b86c1d1aee5
Steven Ng <steven@nomadicoder.com>
2019-11-07 15:45:03 -0500

    Ignore undetermined parameter

--- spec/services/sync_hours_spec.rb
+++ spec/services/sync_hours_spec.rb
@@ -81,6 +81,9 @@ RSpec.describe SyncService::LibraryHours, type: :service do
       VCR.configure do |config|
         config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
         config.hook_into :webmock
+        config.default_cassette_options = {
+          match_requests_on: [:method, VCR.request_matchers.uri_without_param(:key)]
+        }
       end
     end
 

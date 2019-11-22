f6e1b0c2b8c6370764b5c626e815cb6e3112cb0c
Steven Ng <steven@nomadicoder.com>
2019-11-22 14:27:35 -0500

    Make title draftable

--- spec/features/service_draft_spec.rb
+++ spec/features/service_draft_spec.rb
@@ -8,11 +8,11 @@ RSpec.feature "ServiceDrafts", type: :feature do
       @account = FactoryBot.create(:account, admin: true)
       @service = FactoryBot.create(:service)
       login_as(@account, scope: :account)
+      visit("/admin/services/#{@service.id}/edit")
     end
     let(:new_description) { "Don't Panic!" }
 
     scenario "Change the Service Description" do
-      visit("/admin/services/#{@service.id}/edit")
       expect(page).to have_xpath("//div[@id=\"service_description\"]/text()[contains(., \"#{@service.description}\")]")
       expect(page).to have_xpath("//textarea[@id=\"service_draft_description\"]/text()[contains(., \"#{@service.draft_description}\")]")
       find("textarea#service_draft_description").set(new_description)
@@ -28,7 +28,6 @@ RSpec.feature "ServiceDrafts", type: :feature do
     end
 
     scenario "Change the Service Access Description" do
-      visit("/admin/services/#{@service.id}/edit")
       expect(page).to have_xpath("//div[@id=\"service_access_description\"]/text()[contains(., \"#{@service.access_description}\")]")
       expect(page).to have_xpath("//textarea[@id=\"service_draft_access_description\"]/text()[contains(., \"#{@service.draft_access_description}\")]")
       find("textarea#service_draft_access_description").set(new_description)
@@ -44,15 +43,11 @@ RSpec.feature "ServiceDrafts", type: :feature do
     end
 
     scenario "Change the Service Title" do
-      skip
-      visit("/admin/services/#{@service.id}/edit")
-      expect(page).to have_xpath("//input[@id=\"service_title\" and @value=\"#{@service.title}\"]")
-
-      find("input#service_title").set(new_description)
+      find("input#service_draft_title").set(new_description)
       check("Apply Draft")
       click_button("Update Service")
       visit("/admin/services/#{@service.id}/edit")
-      expect(page).to have_xpath("//input[@id=\"service_title\" and @value=\"#{@service.title}\"]")
+      expect(page).to have_xpath("//div[@id=\"service_title\"]/text()[contains(., \"#{new_description}\")]")
     end
   end
 end

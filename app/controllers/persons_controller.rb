261ae28f6c40485ce9b2fc28ccc69e8b1947007e
Chris Doyle <cdoyle@temple.edu>
2020-01-22 13:18:22 -0500

    Updated variable used for JSON delivery.

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -8,11 +8,12 @@ class PersonsController < ApplicationController
 
   def index
     @persons = get_persons
+    json_people = Person.all
     @fcn_link = Webpage.find_by(title: "Frequently called numbers")
 
     respond_to do |format|
       format.html
-      format.json { render json: PersonSerializer.new(@persons.to_a) }
+      format.json { render json: PersonSerializer.new(json_people.to_a) }
     end
   end
 

7f3578e6ace9a0602d19ce18622cde865a70be62
Chris Doyle <cdoyle@temple.edu>
2020-02-13 17:45:06 -0500

    Update default sort order on Highlights dashboard

--- app/controllers/admin/highlights_controller.rb
+++ app/controllers/admin/highlights_controller.rb
@@ -3,5 +3,12 @@
 module Admin
   class HighlightsController < Admin::ApplicationController
     include Admin::Detachable
+
+    def order
+      @order ||= Administrate::Order.new(
+        params.fetch(resource_name, {}).fetch(:order, "created_at"),
+        params.fetch(resource_name, {}).fetch(:direction, "desc"),
+      )
+    end
   end
 end

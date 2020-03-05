0317f76532096c7f458115fdf4d29ab43359a210
Chris Doyle <cdoyle@temple.edu>
2020-03-05 16:13:45 -0500

    Adds Group.all to JSON format response for groups. Remove JSON response formats Remove groups from Swagger

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe GroupsController, type: :controller do
   let(:group) { FactoryBot.create(:group) }
 
   describe "GET #index" do
-    it "returns json when requested" do
+    xit "returns json when requested" do
       get :index, format: :json
       expect(response.header["Content-Type"]).to include "json"
     end
@@ -17,7 +17,7 @@ RSpec.describe GroupsController, type: :controller do
   describe "GET #show as JSON" do
     let(:group) { FactoryBot.create(:group) }
 
-    it "returns valid json" do
+    xit "returns valid json" do
       get :show, format: :json, params: { id: group.id }
       Tempfile.open(["serialized_group", ".json"]) do |serialized|
         serialized.write(response.body)

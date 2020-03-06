23ebd60e807b5199e36c2833517b66ac539f01f3
Chad <chad.nelson@temple.edu>
2020-03-06 14:27:33 -0500

    SImplify the test for generating a sitemap
    Avoids false errors

--- spec/misc/sitemap_generator_spec.rb
+++ spec/misc/sitemap_generator_spec.rb
@@ -9,9 +9,6 @@ RSpec.describe "Running the sitemap generator rake task with our config" do
     Rake::Task["sitemap:clean"].invoke
     Rake::Task["sitemap:create"].invoke
   }
-  it "does not throw an error" do
-    expect { create_sitemap }.not_to raise_error
-  end
   it "puts the sitemap to aws" do
     create_sitemap
     expect(WebMock).to have_requested(:put, /.*\.amazonaws\.com\/sitemap\.xml\.gz/)

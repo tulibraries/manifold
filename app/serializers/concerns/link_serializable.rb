a120409460852c00a6220a7e44a9303ec71d12b5
Chris Doyle <cdoyle@temple.edu>
2020-03-10 10:32:17 -0400

    mend

--- app/serializers/concerns/link_serializable.rb
+++ app/serializers/concerns/link_serializable.rb
@@ -4,7 +4,7 @@ module LinkSerializable
   extend ActiveSupport::Concern
 
   included do
-    link :self, Proc.new { |the_object| helpers.url_for(controller: the_object.model_name.route_key == "people" ? "persons" : the_object.model_name.route_key,
+    link :self, Proc.new { |the_object| helpers.url_for(controller: the_object.model_name.route_key,
                                                         action: :show,
                                                         id: the_object.to_param) }
   end

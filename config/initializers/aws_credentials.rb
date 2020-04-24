2a0d9d96fa9fa2e96cb4b9e5033470ce03722f04
Steven Ng <steven@nomadicoder.com>
2020-04-24 10:52:34 -0400

    Run CircleCI tests in test rails environment
    - Set test environment for all bundle exec calls
    - Require AWS API support for tests
    - Specify dummy values for AWS default values

--- config/initializers/aws_credentials.rb
+++ config/initializers/aws_credentials.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.configuration.aws = {
-  access_key_id: ENV.fetch("S3_ACCESS_KEY_ID", ""),
-  secret_access_key: ENV.fetch("S3_SECRET_ACCESS_KEY", ""),
-  region: ENV.fetch("S3_REGION", ""),
-  bucket: ENV.fetch("S3_BUCKET", "")
+  access_key_id: ENV.fetch("S3_ACCESS_KEY_ID", "access_key_id"),
+  secret_access_key: ENV.fetch("S3_SECRET_ACCESS_KEY", "secret_access_key"),
+  region: ENV.fetch("S3_REGION", "region"),
+  bucket: ENV.fetch("S3_BUCKET", "bucket")
 }

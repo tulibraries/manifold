e190e941e2d40a947a01cfaf34ea6f7000160471
Chris Doyle <cdoyle@temple.edu>
2020-07-15 10:48:39 -0400

    Break ask a librarian out to its own section on top of list of hours.

--- app/helpers/library_hours_helper.rb
+++ app/helpers/library_hours_helper.rb
@@ -2,39 +2,47 @@
 
 module LibraryHoursHelper
   def location_name(slug)
-    location = Building.find_by(hours: slug)
-    unless location.nil?
-      name = location.name
-    end
-    if location.nil?
-      location = Space.find_by(hours: slug)
+    if slug == "online"
+      name = "Online"
+    else
+      location = Building.find_by(hours: slug)
       unless location.nil?
         name = location.name
       end
-    end
-    if location.nil?
-      location = Service.find_by(hours: slug)
-      unless location.nil?
-        name = location.title
+      if location.nil?
+        location = Space.find_by(hours: slug)
+        unless location.nil?
+          name = location.name
+        end
+      end
+      if location.nil?
+        location = Service.find_by(hours: slug)
+        unless location.nil?
+          name = location.title
+        end
       end
     end
     name
   end
   def location_link(slug)
-    location = Building.find_by(hours: slug)
-    unless location.nil?
-      name = link_to location.name, location
-    end
-    if location.nil?
-      location = Space.find_by(hours: slug)
+    if slug == "online"
+      name = "Online"
+    else
+      location = Building.find_by(hours: slug)
       unless location.nil?
         name = link_to location.name, location
       end
-    end
-    if location.nil?
-      location = Service.find_by(hours: slug)
-      unless location.nil?
-        name = link_to location.title, location
+      if location.nil?
+        location = Space.find_by(hours: slug)
+        unless location.nil?
+          name = link_to location.name, location
+        end
+      end
+      if location.nil?
+        location = Service.find_by(hours: slug)
+        unless location.nil?
+          name = link_to location.title, location
+        end
       end
     end
     name

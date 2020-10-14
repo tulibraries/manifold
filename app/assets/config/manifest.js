62fb88cbc03cabd0cf15689355e834f97edaac43
Chris Doyle <cdoyle@temple.edu>
2020-10-14 12:35:33 -0400

    remove redundant migration, remove actiontext.scss, update manifest.js to remove sprockets administrate js assets.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,3 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 //= link_directory ../stylesheets/administrate .scss
-//= link administrate/application.css
-//= link administrate/application.js
--- app/assets/javascripts/administrate/application.js
+++ app/assets/javascripts/administrate/application.js
@@ -3,8 +3,8 @@
 //= require selectize
 //= require moment
 //= require datetime_picker
-//= require trix
 //= require_tree .
+//= require trix
 
 (function() {
   

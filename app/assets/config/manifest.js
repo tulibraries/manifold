bf3aaf8f6aac70ce91ab9a7c33a93dc12f20f39e
Chris Doyle <cdoyle@temple.edu>
2020-10-14 12:40:57 -0400

    Added sprokets administrate assets back to manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,5 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 //= link_directory ../stylesheets/administrate .scss
+//= link administrate/application.css
+//= link administrate/application.js

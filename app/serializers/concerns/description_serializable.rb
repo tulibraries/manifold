635be660fdc8937d25609c5925622795be2ef9a2
Steven Ng <steven@nomadicoder.com>
2020-12-17 16:36:49 -0500

    Serialize rich-text fields

---
+++ app/serializers/concerns/description_serializable.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module DescriptionSerializable
+  extend ActiveSupport::Concern
+
+  included do
+    attribute :description do |the_object|
+      the_object.description.body.to_html
+    end 
+  end
+end

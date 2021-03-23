a4039d9c2547c00112a438ea9402a1bf192f105d
Chris Doyle <chris@gcdoyle.com>
2021-03-23 07:32:58 -0400

    update migration

--- db/migrate/20210318162623_add_epub_to_webpages.rb
+++ db/migrate/20210318162623_add_epub_to_webpages.rb
@@ -2,6 +2,8 @@
 
 class AddEpubToWebpages < ActiveRecord::Migration[6.1]
   def change
-    add_column :webpages, :epub, :string
+    unless column_exists? :webpages, :epub
+      add_column :webpages, :epub, :string
+    end
   end
 end

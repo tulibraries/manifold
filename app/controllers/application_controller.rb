f09b7595a322bb51e89fcfcd4cbe6c699f5dadc0
Chris Doyle <chris@gcdoyle.com>
2021-03-24 09:32:01 -0400

    Remone parent category from about menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,8 @@ class ApplicationController < ActionController::Base
   before_action :show_hours, :menu_items
 
   def menu_items
-    @about_items = Category.find_by(slug: "about-page").items if Category.find_by(slug: "about-page").present?
+    about = Category.find_by(slug: "about-page")
+    @about_items = Category.find_by(slug: "about-page").items(exclude: [about]) if Category.find_by(slug: "about-page").present?
     @visit_items = Category.find_by(slug: "visit").items if Category.find_by(slug: "visit").present?
     @research_items = Category.find_by(slug: "research-services").items if Category.find_by(slug: "research-services").present?
   end

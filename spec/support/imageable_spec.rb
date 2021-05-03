81e8e47125bd3abc23f55328945778c7dd9be946
Chris Doyle <chris@gcdoyle.com>
2021-05-03 11:55:04 -0400

    Add file size test

--- spec/support/imageable_spec.rb
+++ spec/support/imageable_spec.rb
@@ -5,9 +5,10 @@ require "spec_helper"
 RSpec.shared_examples "imageable" do
   let(:model) { described_class } # the class that includes the concern
   let(:factory_model) { FactoryBot.create(model.to_s.underscore.to_sym) }
+  let(:plus_model) { FactoryBot.create(model.to_s.underscore.to_sym) }
   let(:image) {
     file_path = Rails.root.join("spec/fixtures/charles.jpg")
-    file = Rack::Test::UploadedFile.new(file_path, "image/png")
+    file = Rack::Test::UploadedFile.new(file_path, "image/jpeg")
     factory_model.image.attach(file)
   }
 
@@ -16,8 +17,16 @@ RSpec.shared_examples "imageable" do
   end
 
   context "when it has no image" do
-    it "the image attachment returns nil when not assigned" do
+    it "the image attachment returns nil" do
       expect(factory_model.image.attachment).to be_nil
+    end
+  end
+
+  context "when image filesize > 700kb" do
+    file_path = Rails.root.join("spec/fixtures/orecchiette.jpg")
+    file = Rack::Test::UploadedFile.new(file_path, "image/jpeg")
+    it "raises error when filesize is too large" do
+      expect(plus_model.image.attach(file)).to be_falsey
     end
   end
 

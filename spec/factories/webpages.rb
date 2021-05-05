ad96c765122db04145b1cd16a0cb83e49e11fca3
Chris Doyle <chris@gcdoyle.com>
2021-05-05 10:30:00 -0400

    Add test for new pdf buttons

--- spec/factories/webpages.rb
+++ spec/factories/webpages.rb
@@ -5,18 +5,30 @@ FactoryBot.define do
     title { "Webpage" }
     description { ActionText::Content.new("Hello World") }
     layout { "None" }
+    trait :as_virtual_tour do
+      after :create do |webpage|
+        external_link = FactoryBot.create(:external_link)
+        webpage.assign_attributes(title: "Tour Page", external_link: external_link, layout: "Virtual Tour")
+      end
+    end
+    trait :with_external_link do
+      after :create do |webpage|
+        external_link = FactoryBot.create(:external_link)
+        webpage.assign_attributes(external_link: external_link)
+      end
+    end
     trait :with_file do
       after :create do |webpage|
         file = FactoryBot.create(:file_upload)
-        webpage.file_uploads << file
+        webpage.assign_attributes(file_uploads: [file])
       end
     end
     trait :with_files do
       after :create do |webpage|
-        file = FactoryBot.create(:file_upload)
+        file1 = FactoryBot.create(:file_upload)
         file2 = FactoryBot.create(:file_upload)
-        webpage.file_uploads << file
-        webpage.file_uploads << file2
+        webpage.assign_attributes(file_uploads: [file1, file2])
+
       end
     end
   end
--- spec/views/webpages/show.html.erb_spec.rb
+++ spec/views/webpages/show.html.erb_spec.rb
@@ -11,15 +11,30 @@ RSpec.describe "webpages/show", type: :view do
   it "displays the sample webpage pdf" do
     @webpage = FactoryBot.create(:webpage, :with_file)
     render
-    expect(rendered).to match /#{@webpage.file_uploads.first.file.attachment.blob.filename.to_s}/
+    expect(rendered).to render_template(partial: "_attachments")
+    expect(rendered).to match /#{@webpage.file_uploads.first.name}/
+    expect(rendered).to match /-- View PDF/
   end
 
   it "displays multiple sample webpage pdfs" do
     @webpage = FactoryBot.create(:webpage, :with_files)
     render
     @webpage.file_uploads.each do |wp|
-      expect(rendered).to match /#{wp.file.attachment.blob.filename.to_s}/
+      expect(rendered).to match /#{wp.name}/
     end
+  end
+
+  it "displays a virtual tour" do
+    @webpage = FactoryBot.create(:webpage, :as_virtual_tour)
+    render
+    expect(rendered).to render_template(partial: "_virtual-tour")
+    expect(rendered).to match /TOUR NAME/
+  end
+
+  it "displays an external link" do
+    @webpage = FactoryBot.create(:webpage, :with_external_link)
+    render
+    expect(rendered).to match /"#{@webpage.external_link.link}/
   end
 
 end

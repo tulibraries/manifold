ab5dac810fa22163865e93796b9ff74eba24f9b1
Chris Doyle <chris@gcdoyle.com>
2021-07-20 10:05:58 -0400

    Add print styles for pages, adding link text for related links.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,6 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 //= link_directory ../stylesheets/administrate .scss
+//= link print/application.css
 //= link administrate/application.css
 //= link administrate/application.js
-
-//= link print/application.scss

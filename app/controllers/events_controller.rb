b8c0888b683a7665802cf5387cf39af23ed4f659
Chris Doyle <chris@gcdoyle.com>
2021-09-03 08:49:15 -0400

    refactor tags collection in controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -91,7 +91,7 @@ class EventsController < ApplicationController
       @all_events = Event.group(:id)
       @featured_events = Event.where(featured: true).order(:start_time).take(3)
       @today = Date.current
-      @new_tags = t("manifold.events.tags").map{|tag| tag[1]}.sort
+      @new_tags = t("manifold.events.tags").map { |tag| tag[1] }.sort
     end
 
     def set_event

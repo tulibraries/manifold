315d60efcc57b501b7d3af65d0b8752fcb309036
Chris Doyle <chris@gcdoyle.com>
2021-10-01 09:57:22 -0400

    remove restrictions on form/add dependent option for policies rspec fail

--- app/models/concerns/has_policies.rb
+++ app/models/concerns/has_policies.rb
@@ -6,7 +6,7 @@ module HasPolicies
   extend ActiveSupport::Concern
   included do
     scope_by_class_name = name
-    has_many :policy_applications, -> { where(policyable_type: scope_by_class_name) }, foreign_key: :policyable_id, inverse_of: false
+    has_many :policy_applications, -> { where(policyable_type: scope_by_class_name) }, foreign_key: :policyable_id, inverse_of: false, dependent: :nullify
     has_many :policies, through: :policy_applications
   end
 end

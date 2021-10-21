01bfe5afe1131cb7673af5d31f980b775322b3a3
Chris Doyle <chris@gcdoyle.com>
2021-10-21 08:48:17 -0400

    adjust speed of transition

--- app/assets/javascripts/listeners.js
+++ app/assets/javascripts/listeners.js
@@ -5,22 +5,22 @@ $(document).ready(function(){
   $("#nav-sec21a-tab").click(function() {
     $('html, body').animate({
         scrollTop: $("#nav-tabs").offset().top
-    }, 1500);
+    }, 1000);
   });
   $("#nav-sec21b-tab").click(function() {
     $('html, body').animate({
         scrollTop: $("#nav-tabs").offset().top
-    }, 1500);
+    }, 1000);
   });
   $("#nav-sec22-tab").click(function() {
     $('html, body').animate({
         scrollTop: $("#nav-tabs").offset().top
-    }, 1500);
+    }, 1000);
   });
   $("#nav-sec23-tab").click(function() {
     $('html, body').animate({
         scrollTop: $("#nav-tabs").offset().top
-    }, 1500);
+    }, 1000);
   });
 
   setTimeout(function(){

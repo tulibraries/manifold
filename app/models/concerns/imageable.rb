df1fddee2446946e64fa73cb7329e5f818e6cb85
Chris Doyle <chris@gcdoyle.com>
2021-12-09 17:16:00 -0500

    metadata checks

--- app/models/concerns/imageable.rb
+++ app/models/concerns/imageable.rb
@@ -58,15 +58,16 @@ module Imageable
   end
 
   def image_variation(width, height)
-    #
     image.blob.analyze
+
+    binding.pry if image.blob.metadata[:width].nil?
 
     if image.blob.metadata[:width] > image.blob.metadata[:height]
       ActiveStorage::Variation.new(Uploads.resize_x_and_pad(width: width, height: height, blob: image.blob))
     else
       # ActiveStorage::Variation.new(Uploads.resize_to_fit(width: width, height: height, blob: self.send(image_field.to_sym).blob))
       ActiveStorage::Variation.new(Uploads.resize_to_fit(width: width, height: height, blob: image.blob))
-    end if image.blob.present?
+    end if image.blob.metadata[:width].present? && image.blob.metadata[:height]
   end
 
   def entity_image_path(type)

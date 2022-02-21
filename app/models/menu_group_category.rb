a3e9b14d29445f1dbf82e20be42f3b5388d9cf07
Chris Doyle <chris@gcdoyle.com>
2022-02-21 14:09:58 -0500

    rubocop

--- app/models/menu_group_category.rb
+++ app/models/menu_group_category.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class MenuGroupCategory < ApplicationRecord
   belongs_to :category
   belongs_to :menu_group
--- db/migrate/20220218175947_create_menu_group_categories.rb
+++ db/migrate/20220218175947_create_menu_group_categories.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateMenuGroupCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :menu_group_categories do |t|
       t.integer :menu_group_id
-      t.integer :category_id      
+      t.integer :category_id
 
       t.timestamps
     end

501609ba9de1595e80f38ff5a7f60ed41ac5b2c7
Chris Doyle <cdoyle@temple.edu>
2022-08-25 12:30:57 -0400

    rubocop

--- app/controllers/admin/application_failovers_controller.rb
+++ app/controllers/admin/application_failovers_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Admin
   class ApplicationFailoversController < Admin::ApplicationController
     def find_resource(param)
--- app/models/application_failover.rb
+++ app/models/application_failover.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationFailover < ApplicationRecord
   has_rich_text :description
 end

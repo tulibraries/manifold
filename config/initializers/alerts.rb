44a9119e83200519ca02b2242c83692f55475675
Steven Ng <steven@nomadicoder.com>
2022-12-02 10:56:13 -0700

    Ensure public/assets directory

--- config/initializers/alerts.rb
+++ config/initializers/alerts.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.config.after_initialize do
+  system("mkdir", "-p", "public/assets") unless File.directory?("public/assets")
   Alert.publish_json(File.open("public/assets/alerts.json", "w"))
 end

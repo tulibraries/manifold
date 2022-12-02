b345f460c8085f62018be850117ec1776f3ee14d
Steven Ng <steven@nomadicoder.com>
2022-12-02 16:16:37 -0700

    Create an empty alerts json

--- config/initializers/alerts.rb
+++ config/initializers/alerts.rb
@@ -4,4 +4,5 @@ Rails.application.config.after_initialize do
   Publish::Alerts.publish
 #  system("mkdir", "-p", "public/assets") unless File.directory?("public/assets")
 #  Alert.publish_json(File.open("public/assets/alerts.json", "w"))
+   File.open("public/assets/alerts.json", "w") { |file| file.write('{"data":[]}') }
 end

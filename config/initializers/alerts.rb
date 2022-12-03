98fc88a496d61a1d9decc61b8f04847f13d18a1a
Steven Ng <steven@nomadicoder.com>
2022-12-02 17:16:16 -0700

    Diagnose missing database problem with CircleCI

--- config/initializers/alerts.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.config.after_initialize do
-  Publish::Alerts.publish
-#  system("mkdir", "-p", "public/assets") unless File.directory?("public/assets")
-#  Alert.publish_json(File.open("public/assets/alerts.json", "w"))
-   File.open("public/assets/alerts.json", "w") { |file| file.write('{"data":[]}') }
-end

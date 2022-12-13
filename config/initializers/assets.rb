1af60a3379121fb8f88027202df8e140fdcab303
Chris Doyle <cdoyle@temple.edu>
2022-12-13 09:32:49 -0500

    update trix location

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
-Rails.application.config.assets.paths << Rails.root.join("node_modules/trix")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/trix/dist")
 Rails.application.config.assets.paths << Rails.root.join("app/assets/images")
 Rails.application.config.assets.paths << Rails.root.join("app/assets/images")
 Rails.application.config.assets.paths << Rails.root.join("app/assets/stylesheets/administrate")

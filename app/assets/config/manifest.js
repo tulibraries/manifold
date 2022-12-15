3477dab71fbddc00b370e16a2cc24e82a7eb0e8b
Chris Doyle <cdoyle@temple.edu>
2022-12-15 12:46:01 -0500

    image asset paths

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
 //= link_tree ../images
-//= link_tree ../builds 
+//= link_tree ../builds
+//= link_tree ../stylesheets/print
 //= link administrate-field-date_picker/application.css
 //= link administrate-field-date_picker/application.js

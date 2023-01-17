73ba0c4a4cc0146b8e7b0b5f6b55e12cecea17df
Chris Doyle <cdoyle@temple.edu>
2023-01-17 13:28:01 -0500

    rubocop

--- config/application.rb
+++ config/application.rb
@@ -55,9 +55,9 @@ module Manifold
     config.draftable = ENV.fetch("MANIFOLD_DRAFTABLE", "false") == "true"
     config.sync_timeout = ENV.fetch("MANIFOLD_SYNC_TIMEOUT", "180").to_i
 
-    config.active_record.encryption.primary_key = ENV['ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY']
-    config.active_record.encryption.deterministic_key = ENV['ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY']
-    config.active_record.encryption.key_derivation_salt = ENV['ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT']
+    config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
+    config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
+    config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
 
     config.active_record.encryption.support_unencrypted_data = true
   end

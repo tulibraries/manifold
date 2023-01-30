961836df84009f4e0e0c6786aa9e85c0cf608901
Chris Doyle <cdoyle@temple.edu>
2023-01-30 16:25:13 -0500

    move json generation to callable service

--- app/controllers/admin/finding_aids_controller.rb
+++ app/controllers/admin/finding_aids_controller.rb
@@ -2,25 +2,6 @@
 
 module Admin
   class FindingAidsController < Admin::ApplicationController
-    after_action :create_json_file, only: [:update, :destroy, :create]
     include Admin::Draftable
-
-    def create
-      super
-    end
-
-    def update
-      super
-    end
-
-    def destroy
-      super
-    end
-
-    private
-
-      def create_json_file
-        File.open("public/finding_aids.json", "w") { |file| file.write(FindingAidSerializer.new(FindingAid.all).serializable_hash.to_json) }
-      end
   end
 end

507f059903681c1ef2174c940851e0bec41cdb3e
Chris Doyle <cdoyle@temple.edu>
2023-08-11 14:11:12 -0400

    remove draftable for snippets

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 class Snippet < ApplicationRecord
-  include Draftable
   extend FriendlyId
   friendly_id :title, use: [:slugged, :finders]
   friendly_id :slug_candidates, use: :slugged
 
   has_rich_text :description
-  has_rich_text :draft_description
   validates :description, presence: true
 
   def slug_candidates

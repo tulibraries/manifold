28df8c07aaba126400b92ce3ca33e2dec100c028
Chris Doyle <cdoyle@temple.edu>
2023-09-14 08:30:01 -0400

    force nil to empty string

--- app/helpers/buildings_helper.rb
+++ app/helpers/buildings_helper.rb
@@ -2,7 +2,7 @@
 
 module BuildingsHelper
   def phone_formatted(phone_number = "")
-    digits = phone_number.gsub(/\D/, "")
+    digits = phone_number.to_s.gsub(/\D/, "")
     if digits.size == 10
       number_to_phone(digits, area_code: true)
     else

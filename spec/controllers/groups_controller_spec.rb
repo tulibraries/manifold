ce7d17b152df79d93c9332205b76bbe93750c3c6
Steven Ng <steven.ng@temple.edu>
2023-10-04 10:15:04 -0600

    Add group controller specs

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -8,11 +8,16 @@ RSpec.describe GroupsController, type: :controller do
   let(:group) { FactoryBot.create(:group) }
 
   describe "GET #index" do
-    xit "returns json when requested" do
-      # we do not provide serialization for groups
-      get :index, format: :json
-      expect(response.header["Content-Type"]).to include "json"
+    it "redirects to home page" do
+      get :index
+      expect(response).to redirect_to "/"
     end
   end
 
+  describe "GET #show" do
+    it "redirects to home page" do
+      get :show, params: { id: group.id }
+      expect(response).to redirect_to "/"
+    end
+  end
 end

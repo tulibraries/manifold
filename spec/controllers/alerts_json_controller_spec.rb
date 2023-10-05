f7b2e517a5a0572b10df54abc500d2bcc9041cbf
Steven Ng <steven.ng@temple.edu>
2023-10-05 07:57:58 -0600

    Replace lazy loading with explicitly creating alerts_json record
    - Lazy loading failed to load in most instsances during local tests

--- spec/controllers/alerts_json_controller_spec.rb
+++ spec/controllers/alerts_json_controller_spec.rb
@@ -3,12 +3,12 @@
 require "rails_helper"
 
 RSpec.describe AlertsJsonController, type: :controller do
-  let (:message) { "Test Message" }
-  let (:alerts_json) { FactoryBot.create(:alerts_json) }
+  before (:all) do
+    @alerts_json = FactoryBot.create(:alerts_json)
+  end
 
   describe "GET #index" do
     it "returns json when requested" do
-      alerts_json.update(message: "#{message}")
       get :index, format: :json
       expect(response).to have_http_status(:ok)
     end

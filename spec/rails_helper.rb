3b25bdfb2e11c1486552bafef05bee2405bfcc30
Chris Doyle <cdoyle@temple.edu>
2023-10-06 09:49:22 -0400

    set dbcleaner to always use transactions

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -49,22 +49,22 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
   config.before(:suite) do
-    if config.use_transactional_fixtures?
-      raise(<<-MSG)
-        Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
-        (or set it to false) to prevent uncommitted transactions being used in
-        JavaScript-dependent specs.
+    # if config.use_transactional_fixtures?
+    #   raise(<<-MSG)
+    #     Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
+    #     (or set it to false) to prevent uncommitted transactions being used in
+    #     JavaScript-dependent specs.
 
-        During testing, the app-under-test that the browser driver connects to
-        uses a different database connection to the database connection used by
-        the spec. The app's database connection would not be able to access
-        uncommitted transaction data setup over the spec's database connection.
-      MSG
-    end
-    DatabaseCleaner.clean_with(:truncation)
+    #     During testing, the app-under-test that the browser driver connects to
+    #     uses a different database connection to the database connection used by
+    #     the spec. The app's database connection would not be able to access
+    #     uncommitted transaction data setup over the spec's database connection.
+    #   MSG
+    # end
+    DatabaseCleaner.clean_with(:transaction)
   end
 
   config.before(:each) do

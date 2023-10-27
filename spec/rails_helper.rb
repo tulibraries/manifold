c6fd8f4e0fb94b7b4a28e12d46b8e374dca32f11
Chris Doyle <cdoyle@temple.edu>
2023-10-27 13:43:06 -0400

    remove stub

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -141,8 +141,8 @@ RSpec.configure do |config|
     stub_request(:get, "https://sites.temple.edu/devopsing/feed").
       to_return(status: 200, body: File.open("#{fixture_path}/blog_posts.rss") , headers: {})
 
-    stub_request(:put, "https://bucket.s3.region.amazonaws.com/sitemap.xml.gz").
-      to_return(status: 200, body: File.open("#{fixture_path}/vcr_cassettes/etexts.yml"), headers: {})
+    # stub_request(:put, "https://bucket.s3.region.amazonaws.com/sitemap.xml.gz").
+    #   to_return(status: 200, body: "", headers: {})
 
 
 

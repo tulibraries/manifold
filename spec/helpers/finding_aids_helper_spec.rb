57b344ac3150ed1618b5a12758f7e14893d4ba03
Steven Ng <steven.ng@temple.edu>
2023-10-31 08:25:58 -0700

    Cover all paths in finding_aid_helper:subject_link method

--- spec/helpers/finding_aids_helper_spec.rb
+++ spec/helpers/finding_aids_helper_spec.rb
@@ -8,4 +8,35 @@ RSpec.describe FindingAidsHelper, type: :helper do
       expect(subject_links([])).to match_array([])
     end
   end
+
+  describe "It has a subject" do
+    let (:subject) { "subject1" }
+    it "returns a link" do
+      expect(subject_links([subject])).to have_content(subject)
+    end
+  end
+
+  describe "It has chosen subjects" do
+    let(:subject) { "subject1" }
+    let(:params) { {
+        subject:
+      } }
+    it "returns a link" do
+      link = subject_links([subject])
+      expect(link.first).to have_content(subject)
+    end
+  end
+
+  describe "It doesn't have chosen subject" do
+    let(:subject) { "subject1" }
+    let(:chosen_subject) { "subject2" }
+    let(:params) { {
+        subject: chosen_subject
+      } }
+    it "returns a link" do
+      link = subject_links([subject])
+      expect(link.first).to have_content(subject)
+      expect(link.first).to match(/#{chosen_subject}/)
+    end
+  end
 end

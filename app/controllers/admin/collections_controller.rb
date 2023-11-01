c67975f63985db779ee66bd01a7f2b1d65968778
Chris Doyle <cdoyle@temple.edu>
2023-11-01 14:44:29 -0400

    add dependent check with message and test

--- app/controllers/admin/collections_controller.rb
+++ app/controllers/admin/collections_controller.rb
@@ -4,5 +4,28 @@ module Admin
   class CollectionsController < Admin::ApplicationController
     include Admin::Detachable
     include Admin::Draftable
+    def destroy
+      collection = requested_resource
+      collections = Collection.all.select { |collection|
+        collection.finding_aids.present?
+      }
+      if collections
+        links = []
+        collections.each do |collection|
+          collection.finding_aids.take(5).each do |fa|
+            links << "<a href=/admin/finding_aids/#{fa.slug}/edit>#{fa.label}</a>"
+          end
+        end
+
+        notice = "<p class=text-start><strong>#{collection.label}</strong> could not be deleted.<br />"
+        notice += "It still has finding aids attached.<br />"
+        notice += "<p>There are too many to list here.</p>"
+        notice += "<p>If you require help, please contact the help desk at tuhelp@temple.edu to request a data migration.</p>"
+        notice += "<p class='text-center'><a href='/admin/finding_aids'>Finding Aids</a></p>"
+        redirect_to :admin_collections, notice:
+      else
+        super
+      end
+    end
   end
 end
--- spec/controllers/admin/collections_controller_spec.rb
+++ spec/controllers/admin/collections_controller_spec.rb
@@ -3,22 +3,17 @@
 require "rails_helper"
 
 RSpec.describe Admin::CollectionsController, type: :controller do
-
-  # This should return the minimal set of attributes required to create a valid
-  # Collection. As you add validations to Collection, be sure to
-  # adjust the attributes here as well.
   let(:valid_attributes) {
     skip("Add a hash of attributes valid for your model")
   }
-
   let(:invalid_attributes) {
     skip("Add a hash of attributes invalid for your model")
   }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # CollectionsController. Be sure to keep this updated too.
   let(:valid_session) { {} }
+
+  let(:account) { FactoryBot.create(:account, admin: true) }
+  let(:finding_aid) { FactoryBot.create(:finding_aid) }
+  let!(:collection) { FactoryBot.create(:collection, finding_aids: [finding_aid]) }
 
   describe "GET #index" do
     it "returns a success response" do
@@ -36,4 +31,12 @@ RSpec.describe Admin::CollectionsController, type: :controller do
     end
   end
 
+  describe "DELETE #destroy" do
+    render_views true
+    it "does not allow collection deleted if finding_aids attached" do
+      sign_in(account)
+      expect { delete :destroy, params: { id: collection.id } }.to_not change(Collection, :count)
+      expect(flash[:notice]).to match(collection.name)
+    end
+  end
 end

fcdb819b22f4e73add1064b9c7941dbca6ccde9f
sensei100 <tuh42082@temple.edu>
2023-11-27 15:49:57 -0500

    MAN-1358 Update scope for alerts picked up by librarysearch

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -4,5 +4,5 @@ class Alert < ApplicationRecord
   has_paper_trail
   has_rich_text :description
 
-  scope :for_librarysearch, -> { where(published: true).where(for_header: true) }
+  scope :for_librarysearch, -> { where(published: true).where(for_header: false) }
 end

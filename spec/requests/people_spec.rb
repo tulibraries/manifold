e948a4ca0d35255dd761d29cb842021c7a50f31c
Chris Doyle <cdoyle@temple.edu>
2023-12-11 12:43:48 -0500

    fix failing specs after update to system tests

--- spec/requests/people_spec.rb
+++ spec/requests/people_spec.rb
@@ -4,36 +4,44 @@ require "rails_helper"
 
 RSpec.describe "People", type: :request do
 
-  context "People Views" do
+  Subject.destroy_all
+  let(:person) { FactoryBot.create(:person, :with_image) }
+  let(:person2) { FactoryBot.create(:person, :with_image) }
+  let(:group) { FactoryBot.create(:group, persons: [person2]) }
+  let!(:specialist) { FactoryBot.create(:person, :with_image, :with_subjects) }
+
+  describe "People Views" do
     it "renders the index page" do
-      person = FactoryBot.create(:person, :with_image)
-      specialist = FactoryBot.create(:person, :with_image, :with_subjects)
       get people_path
       expect(response).to render_template(:index)
       expect(response.body).to include(specialist.label)
     end
 
     it "renders the specialists" do
-      # Tests persons controller specialty filter
-      person = FactoryBot.create(:person, :with_image)
-      specialist = FactoryBot.create(:person, :with_image, :with_subjects)
-      get people_path + "?specialty=" + Subject.last.name
-      expect(response).to render_template(:index)
+      get people_path, params: { specialty: Subject.last.name }
       expect(response.body).to include(specialist.label)
       expect(response.body).not_to include(person.label)
     end
 
+    it "filters department present" do
+      get people_path, params: { department: group.friendly_id }
+      expect(response.body).to include(person2.label)
+      expect(response.body).not_to include(person.label)
+    end
+
+    it "filter by search term" do
+      get people_path, params: { search: person2.name }
+      expect(response.body).to include(person2.label)
+      expect(response.body).not_to include(person.label)
+    end
+
     it "renders the show page" do
-      person = FactoryBot.create(:person, :with_image)
-      specialist = FactoryBot.create(:person, :with_image, :with_subjects)
       get person_path(person)
       expect(response).to render_template(:show)
       expect(response.body).to include(person.label)
     end
 
     it "renders the print page" do
-      person = FactoryBot.create(:person, :with_image)
-      specialist = FactoryBot.create(:person, :with_image, :with_subjects)
       get specialists_print_path
       expect(response).to render_template(:specialists_print)
       expect(response.body).to include(specialist.label)
--- spec/requests/top_menu_spec.rb
+++ spec/requests/top_menu_spec.rb
@@ -5,22 +5,20 @@ require "rails_helper"
 RSpec.feature "TopMenu", type: :request do
   describe "weighted menu navigation" do
     context "weighted entries in main menu group" do
-      let(:category_1) { FactoryBot.create(:category, name: "Category 1") }
-      let(:category_2) { FactoryBot.create(:category, name: "Category 2") }
-      let(:webpage) { FactoryBot.create(:webpage, categories: [category_2]) }
+      # menu_group_categories have cateogries and are assigned to menu_groups (which are the main nav menu items in the site)
+      let(:category_1) { FactoryBot.create(:category, name: "Alphaville") }
+      let(:category_2) { FactoryBot.create(:category, name: "Zetatopolis") }
       let(:menu_group) { FactoryBot.create(:menu_group, slug: "about-page", categories: [category_1, category_2]) }
-      let(:menu_group_category_1) { FactoryBot.create(:menu_group_category, menu_group:, category: category_1) }
+      let(:menu_group_category_1) { FactoryBot.create(:menu_group_category, menu_group:, category: category_1, weight: 2) }
       let(:menu_group_category_2) { FactoryBot.create(:menu_group_category, menu_group:, category: category_2, weight: 1) }
 
-
       it "lists weighted categories in order" do
-        category_2.menu_group_id = menu_group.id
-        get webpage_path(webpage)
-        within(".menu-items") do
-          expect(page).to have_content(category_1.name)
-          expect(page).to have_content(category_2.name)
-          expect(category_2.name).to appear_before(category_1.name, only_text: true)
-        end
+        menu_group_category_1.menu_group_id = menu_group.id
+        menu_group_category_2.menu_group_id = menu_group.id
+        get root_path
+        expect(response.body).to match(category_1.name)
+        expect(response.body).to match(category_2.name)
+        response.body.index(category_2.name).should < response.body.index(category_1.name)
       end
     end
   end

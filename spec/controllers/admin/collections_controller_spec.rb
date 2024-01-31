4c9de27d05e626d257c60bd2fa6254b7d3a4f063
Steven Ng <steven.ng@temple.edu>
2024-01-31 14:55:20 -0800

    Remove pending session specs for controllers

--- spec/controllers/admin/collections_controller_spec.rb
+++ spec/controllers/admin/collections_controller_spec.rb
@@ -3,33 +3,9 @@
 require "rails_helper"
 
 RSpec.describe Admin::CollectionsController, type: :controller do
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-  let(:valid_session) { {} }
-
   let(:account) { FactoryBot.create(:account, admin: true) }
   let(:finding_aid) { FactoryBot.create(:finding_aid) }
   let!(:collection) { FactoryBot.create(:collection, finding_aids: [finding_aid]) }
-
-  describe "GET #index" do
-    it "returns a success response" do
-      Collection.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET #show" do
-    it "returns a success response" do
-      collection = Collection.create! valid_attributes
-      get :show, params: { id: collection.to_param }, session: valid_session
-      expect(response).to be_successful
-    end
-  end
 
   describe "DELETE #destroy" do
     render_views true
--- spec/controllers/exhibitions_controller_spec.rb
+++ spec/controllers/exhibitions_controller_spec.rb
@@ -3,14 +3,6 @@
 require "rails_helper"
 
 RSpec.describe ExhibitionsController, type: :controller do
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # ExhibitionsController. Be sure to keep this updated too.
@@ -19,12 +11,6 @@ RSpec.describe ExhibitionsController, type: :controller do
   let(:exhibition) { FactoryBot.create(:exhibition) }
 
   describe "GET #index" do
-    it "returns a success response" do
-      Exhibition.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response).to be_successful
-    end
-
     it "returns json when requested" do
       get :index, format: :json
       expect(response.header["Content-Type"]).to include "json"

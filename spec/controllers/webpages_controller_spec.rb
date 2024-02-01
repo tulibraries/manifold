fab027b5b5e636733e95b79f744d14eddfdf2d3a
Steven Ng <steven.ng@temple.edu>
2024-02-01 09:50:03 -0800

    Remove spec for non-existent views

--- spec/controllers/webpages_controller_spec.rb
+++ spec/controllers/webpages_controller_spec.rb
@@ -10,11 +10,6 @@ RSpec.describe WebpagesController, type: :controller do
   let(:webpage) { FactoryBot.create(:webpage) }
 
   describe "GET #index" do
-    it "returns a success response", skip: "TBA: No views exist" do
-      get :index
-      expect(response).to be_successful
-    end
-
     it "returns json when requested" do
       get :index, format: :json
       expect(response.header["Content-Type"]).to include "json"
@@ -23,11 +18,6 @@ RSpec.describe WebpagesController, type: :controller do
 
   describe "GET #show" do
     let(:webpage) { FactoryBot.create(:webpage) }
-
-    it "returns a success response", skip: "TBA: No views exist" do
-      get :show, params: { id: webpage.to_param }
-      expect(response).to be_successful
-    end
 
     it "returns html by default success" do
       get :show, params: { id: webpage.id }, format: :json

8375a5370899ed487e3e7950b976b8256f8e391b
Steven Ng <steven.ng@temple.edu>
2024-02-01 09:33:36 -0800

    Remove pending requests specs

--- spec/requests/spaces_spec.rb
+++ spec/requests/spaces_spec.rb
@@ -4,24 +4,6 @@ require "rails_helper"
 
 RSpec.describe "Spaces", type: :request do
   let(:building) { FactoryBot.create(:building) }
-  describe "GET /spaces" do
-    it "renders the space home page with all the spaces" do
-      skip "Are these necessary?"
-      space = FactoryBot.create(:space, building_id: building.id)
-      get spaces_path
-      expect(response).to render_template(:index)
-      expect(response.body).to include(space.name)
-    end
-
-    it "works! (now write some real specs)" do
-      skip "Are these necessary?"
-      space = FactoryBot.create(:space, building_id: building.id)
-      get spaces_path + "/#{space.id}"
-      expect(response).to render_template(:show)
-      expect(response.body).to include(space.name)
-    end
-  end
-
   describe "a redirect with a legacy path starting with /space" do
     let(:redirect) { FactoryBot.create(:space_redirect) }
     it "redirects to the expected redirect path" do

7e68e3351a18188cd05e7e638f69fc0a4cc6da0c
Chris Doyle <cdoyle@temple.edu>
2024-02-08 10:17:08 -0500

    refactor email helper to not overwrite fields passed in

--- spec/support/form_helper.rb
+++ spec/support/form_helper.rb
@@ -7,35 +7,24 @@ RSpec.shared_examples "email form" do
     ActionMailer::Base.deliveries = []
   end
 
-  # let(:title) { params[:title] }
-  # let(:recipients) { params[:recipients] }
   let(:the_email) { ActionMailer::Base.deliveries.first }
-  let(:params) { {
-        form_type:,
-        title:,
-        recipients:,
-        name: "test",
-        email: "test@example.com"
-      }.merge(form_params || {})}
-
 
   describe "" do
     it "renders the form" do
-      get "/forms/#{form_type}", params: form_params
+      get("/forms/#{form_type}", params: form_params)
       expect(response).to render_template(:show)
-      expect(response.body).to include(params[:title])
+      expect(response.body).to include(form_params[:form][:title])
     end
 
     it "accepts information" do
-      post(forms_path, params:)
-      expect(the_email.subject).to eq(params[:title])
-      # Test for all form params in email body except recipients or form_type
-      form_params.first.second.each do |key, value|
+      post(forms_path, params: form_params)
+      expect(the_email.subject).to eq(form_params[:form][:title])
+      # Recipients and form_type not included in email body
+      form_params[:form].each do |key, value|
         expect(the_email.body.raw_source).to include(value) unless [:recipients, :form_type].include? key
       end
-      # Check that after the email has been delivered, the
-      # form persists to the db. Hard to do in isolation
-      expect(FormSubmission.take.form_type).to eq(params[:form_type])
+      # Check that the form submission persists to the db.
+      expect(FormSubmission.take.form_type).to eq(form_params[:form][:form_type])
     end
   end
 end

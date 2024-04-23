946c0c3dfbdfa5cb268927acee04633f35dbb00f
Chris Doyle <cdoyle@temple.edu>
2024-04-23 09:40:19 -0400

    use only current collection in calculation

--- app/controllers/admin/collections_controller.rb
+++ app/controllers/admin/collections_controller.rb
@@ -6,20 +6,17 @@ module Admin
     include Admin::Draftable
     def destroy
       collection = requested_resource
-      collections = Collection.all.select { |collection|
-        collection.finding_aids.present?
-      }
-      if collections
+      if collection.finding_aids.present?
         links = []
-        collections.each do |collection|
-          collection.finding_aids.take(5).each do |fa|
-            links << "<a href=/admin/finding_aids/#{fa.slug}/edit>#{fa.label}</a>"
-          end
+        collection.finding_aids.take(5).each do |fa|
+          links << "<a href='/admin/finding_aids/#{fa.slug}/edit'>#{fa.label}</a><br />"
         end
 
         notice = "<p class=text-start><strong>#{collection.label}</strong> could not be deleted.<br />"
         notice += "It still has finding aids attached.<br />"
-        notice += "<p>There are too many to list here.</p>"
+        links.each do |link|
+          notice += link
+        end
         notice += "<p>If you require help, please contact the help desk at tuhelp@temple.edu to request a data migration.</p>"
         notice += "<p class='text-center'><a href='/admin/finding_aids'>Finding Aids</a></p>"
         redirect_to :admin_collections, notice:

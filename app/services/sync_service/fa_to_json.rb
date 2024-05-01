8a75d887c820b1a367e038dbc3245dcc4f7f9f9a
Chris Doyle <cdoyle@temple.edu>
2024-05-01 14:56:50 -0400

    change json location

--- app/services/sync_service/fa_to_json.rb
+++ app/services/sync_service/fa_to_json.rb
@@ -2,6 +2,6 @@
 
 class SyncService::FaToJson
   def self.call
-    File.open("public/finding_aids.json", "w") { |file| file.write(FindingAidSerializer.new(FindingAid.all).serializable_hash.to_json) }
+    File.open("public/cache/finding_aids.json", "w") { |file| file.write(FindingAidSerializer.new(FindingAid.all).serializable_hash.to_json) }
   end
 end

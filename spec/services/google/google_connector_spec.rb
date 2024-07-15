2783865db905808b7612b0283b17d13184e4e57b
Chris Doyle <cdoyle@temple.edu>
2024-07-15 09:50:16 -0400

    add scope to connector test

--- spec/services/google/google_connector_spec.rb
+++ spec/services/google/google_connector_spec.rb
@@ -9,10 +9,18 @@ RSpec.describe Google::SheetsConnector, type: :service do
       Google::SheetsConnector.call(feature: "hours")
     end
   }
+  let(:todays_hours) {
+    VCR.use_cassette("todays_hours", match_requests_on: [:method, :without_api_key]) do
+      Google::SheetsConnector.call(feature: "hours", scope: "charles")
+    end
+  }
 
   context "Retrieves hours data from API" do
     it "gets all hours" do
       expect(hours.values.size).to be > 0
+    end
+    it "gets location specific hours" do
+      expect(todays_hours.value_ranges[0].values.size).to be > 0
     end
   end
 

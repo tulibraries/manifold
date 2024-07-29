0e174bd183f26b052bf68b182d5e4188bbed9c11
Chris Doyle <cdoyle@temple.edu>
2024-07-29 09:56:59 -0400

    also add to dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,13 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  config.active_storage.variable_content_types = %w(
+    image/png
+    image/gif
+    image/jpg
+    image/jpeg
+    image/webp
+    )
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false

c77c92570c16098624b3700ff0f023c26a0ea217
Steven Ng <steven.ng@temple.edu>
2024-08-09 16:34:10 -0600

    Restore resource_params in application_controller
    - Override application_controller in redirect admin controller

--- app/controllers/admin/redirects_controller.rb
+++ app/controllers/admin/redirects_controller.rb
@@ -5,5 +5,11 @@ module Admin
     def find_resource(param)
       scoped_resource.find(param)
     end
+
+    def resource_params
+      params.require(resource_class.model_name.param_key)
+        .permit(dashboard.permitted_attributes(action_name))
+        .transform_values { |v| read_param_value(v) }
+    end
   end
 end

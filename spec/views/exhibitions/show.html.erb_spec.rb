34ad84224c5e46621b4d1b604e2e27d7bf3e37b7
Chris Doyle <cdoyle@temple.edu>
2024-09-27 15:44:37 -0400

    adds online_url field, updates model to handle online exhibitions

--- spec/views/exhibitions/show.html.erb_spec.rb
+++ spec/views/exhibitions/show.html.erb_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe "exhibitions/show", type: :view do
     render
     expect(rendered).to match /#{@exhibition.images.first.blob.filename}/
   end
+
+  xit "renders script for exhibition" do
+    @exhibition = FactoryBot.create(:exhibition, online_url: "https://library.temple.edu")
+    render
+    binding.pry
+    exhibition_ld = JSON.parse(Nokogiri::XML(rendered).xpath("//script").text)
+    expect(exhibition_ld["name"]).to match("Online")
+  end
+
+  it "displays online info" do
+    @exhibition = FactoryBot.create(:exhibition, online_url: "https://library.temple.edu")
+    render
+    expect(rendered).to match /#{@exhibition.online_url}/
+  end
 end

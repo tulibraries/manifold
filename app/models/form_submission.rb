df0ebca23e2d2058dfe58abc528d177a28909409
Chris Doyle <cdoyle@temple.edu>
2024-11-12 09:51:10 -0500

    update serialization method for form submissions

--- app/models/form_submission.rb
+++ app/models/form_submission.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class FormSubmission < ApplicationRecord
-  serialize :form_attributes, JSON
+  store :form_attributes, coder: JSON
   has_encrypted :form_attributes
 end

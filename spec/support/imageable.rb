85296ebe1828ac47026c6b2b31f9aee271fd006f
Steven Ng <steven.ng@temple.edu>
2024-12-12 09:48:49 -0700

    Test image upload validation

--- spec/support/imageable.rb
+++ spec/support/imageable.rb
@@ -26,7 +26,17 @@ RSpec.shared_examples "imageable" do
     file_path = Rails.root.join("spec/fixtures/orecchiette.jpg")
     file = Rack::Test::UploadedFile.new(file_path, "image/jpeg")
     it "raises error when filesize is too large" do
-      expect(plus_model.image.attach(file)).to be_falsey
+      plus_model.image.attach(file)
+      expect(plus_model.errors.full_messages).to include(/is larger than 700KB limit./)
+    end
+  end
+
+  context "when image is invalid content type" do
+    file_path = Rails.root.join("spec/fixtures/guidelines.pdf")
+    file = Rack::Test::UploadedFile.new(file_path, "application/pdf")
+    it "raises error when file content type is invalid" do
+      plus_model.image.attach(file)
+      expect(plus_model.errors.full_messages).to include(/has an invalid content type./)
     end
   end
 end

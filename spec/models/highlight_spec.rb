ecaa7c1e3313ad6878cdc17597fe41e805152b44
Chris Doyle <cdoyle@temple.edu>
2025-02-11 09:56:00 -0500

    update tests

--- spec/models/highlight_spec.rb
+++ spec/models/highlight_spec.rb
@@ -8,4 +8,30 @@ RSpec.describe Highlight, type: :model do
     it { should validate_presence_of(:title) }
   end
 
+  describe "with_image scope" do
+    before do
+      @h1 = FactoryBot.create(:highlight, :with_image)
+      @h2 = FactoryBot.create(:highlight)
+    end
+
+    it "only returns highlights with images attached" do
+      expect(Highlight.with_image).to include(@h1)
+      expect(Highlight.with_image).to_not include(@h2)
+    end
+  end
+
+  describe "digcol promoted scope" do
+    before do
+      @h1 = FactoryBot.create(:highlight, :with_image, promote_to_dig_col: true)
+      @h2 = FactoryBot.create(:highlight, promote_to_dig_col: true)
+      @h3 = FactoryBot.create(:highlight, :with_image)
+    end
+
+    it "only returns promoted highlights with images attached" do
+      expect(Highlight.with_image.for_digital_collections).to include(@h1)
+      expect(Highlight.with_image.for_digital_collections).to_not include(@h2)
+      expect(Highlight.with_image.for_digital_collections).to_not include(@h3)
+    end
+  end
+
 end

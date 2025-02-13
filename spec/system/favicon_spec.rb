4a9992cbe25d43b016770320a2153a68978d9533
Chris Doyle <cdoyle@temple.edu>
2025-02-13 09:43:59 -0500

    remove unnecessary instance variables from test

--- spec/system/favicon_spec.rb
+++ spec/system/favicon_spec.rb
@@ -3,12 +3,6 @@
 require "rails_helper"
 
 RSpec.describe "favicon", type: :system do
-
-  before(:all) do
-    @highlight_with_image = FactoryBot.create(:highlight, :with_image, title: "Digcol with Image", promote_to_dig_col: true)
-    @highlight_without_image = FactoryBot.create(:highlight, title: "Digcol without Image", promote_to_dig_col: true)
-  end
-
   describe "favicon" do
     scenario "is present" do
       visit("/contact-us")

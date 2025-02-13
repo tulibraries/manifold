f3887da674f1fae31144792149522320fc7d6ecf
Chris Doyle <cdoyle@temple.edu>
2025-02-13 09:36:15 -0500

    re-enable existing favicon file, add test for presence

---
+++ spec/system/favicon_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "favicon", type: :system do
+
+  before(:all) do
+    @highlight_with_image = FactoryBot.create(:highlight, :with_image, title: "Digcol with Image", promote_to_dig_col: true)
+    @highlight_without_image = FactoryBot.create(:highlight, title: "Digcol without Image", promote_to_dig_col: true)
+  end
+
+  describe "favicon" do
+    scenario "is present" do
+      visit(root_path)
+      expect(page).to have_selector('head link[rel="icon"]', visible: false)
+    end
+  end
+end

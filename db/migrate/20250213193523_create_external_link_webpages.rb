1bb1ea320286fb69f2e3b9d912f65296c0e4626a
Chris Doyle <cdoyle@temple.edu>
2025-02-13 16:02:09 -0500

    db migrations for through table

---
+++ db/migrate/20250213193523_create_external_link_webpages.rb
@@ -0,0 +1,10 @@
+class CreateExternalLinkWebpages < ActiveRecord::Migration[7.2]
+  def change
+    create_table :external_link_webpages do |t|
+      t.references :webpage, null: false, foreign_key: true
+      t.references :external_link, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

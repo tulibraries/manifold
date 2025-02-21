abcaa58ac99dc7ad290e4782ccec237f54133d71
Chris Doyle <cdoyle@temple.edu>
2025-02-21 09:11:59 -0500

    update views and helper to separate events from workshops and cleanup code

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -9,62 +9,74 @@ module EventHelper
     end
   end
 
-  def set_header(type)
+  def set_header(type, filter)
     case type
     when "dss_events"
       t("manifold.events.headings.dsc")
     when "hsl_events"
       t("manifold.events.headings.hsl")
     when "index"
-      t("manifold.events.headings.upcoming_events")
-    when "past"
-      t("manifold.events.headings.past_events")
-    when "upcoming_workshops"
+      if filter.present? && filter == "events-only"
+        t("manifold.events.headings.events")
+      else
+        t("manifold.events.headings.upcoming_events_workshops")
+      end
+    when "past_events"
+      if filter.present? && filter == "events-only"
+        t("manifold.events.headings.past_events")
+      else
+        t("manifold.events.headings.past_events_workshops")
+      end
+    when "workshops"
       t("manifold.events.headings.upcoming_workshops")
     when "past_workshops"
       t("manifold.events.headings.past_workshops")
-    end
-  end
+    when "past_search"
 
-  def workshops_link(type)
-    if type.blank?
-      action_name == "past" ?
-        (link_to "Limit to Workshops", past_events_path(page: 1, type: "workshop", anchor: "list"), class: "workshops-link")
-        :
-        (link_to "Limit to Workshops", events_path(page: 1, type: "workshop", anchor: "list"), class: "workshops-link")
-    else
-      if type == "dss_events" || type == "hsl_events"
-        link_to "View all Workshops", events_path(page: 1, type: "workshop", anchor: "list"), class: "workshops-link"
-      end
-    end
-  end
-
-  def events_link(type)
-    if type.blank?
-      action_name == "past" ?
-        (link_to "Non-Workshop Events", past_events_path(page: 1, type: "non-workshop", anchor: "list"), class: "not-workshops-link")
-        :
-        (link_to "Non-Workshop Events", events_path(page: 1, type: "non-workshop", anchor: "list"), class: "not-workshops-link")
-    else
-      if type == "dss_events" || type == "hsl_events"
-        link_to "View all events", events_path(page: 1, type: "non-workshop", anchor: "list"), class: "not-workshops-link"
-      end
     end
   end
 
   def filters_link(type)
-    if type.blank?
-      workshops_link(type) + " | " + events_link(type)
-    elsif type == "workshop"
-      events_link(nil)
-    elsif type == "non-workshop"
-      workshops_link(nil)
+    (["past", "past_events", "past_search", "past_workshops"].include? type) ?
+    (past_workshops_link(type) + " | " + past_events_link(type)) :
+     (workshops_link(type) + " | " + events_link(type))
+  end
+
+  def workshops_link(type)
+    if type == "dss_events" || type == "hsl_events"
+      link_to "View all Workshops", events_path(anchor: "list"), class: "workshops-link"
+    else
+      link_to "View Workshops", workshops_path(anchor: "list"), class: "workshops-link"
+    end
+  end
+
+  def past_workshops_link(type)
+    if type == "dss_events" || type == "hsl_events"
+      link_to "View All Past Workshops", past_workshops_path(anchor: "list"), class: "workshops-link"
+    else
+      link_to "View Past Workshops", past_workshops_path(anchor: "list"), class: "workshops-link"
+    end
+  end
+
+  def events_link(type)
+    if type == "dss_events" || type == "hsl_events"
+      link_to "View All Events", events_path(type: "events-only", anchor: "list"), class: "not-workshops-link"
+    else
+      link_to "View Events", events_path(type: "events-only", anchor: "list"), class: "not-workshops-link"
+    end
+  end
+
+  def past_events_link(type)
+    if type == "dss_events" || type == "hsl_events"
+      link_to "View All Past Events", past_events_path(type: "events-only", anchor: "list"), class: "not-workshops-link"
+    else
+      link_to "View Past Events", past_events_path(type: "events-only", anchor: "list"), class: "not-workshops-link"
     end
   end
 
   def current_link(type)
-    type == "past_search" ?
-      (link_to "View current events & exhibits", events_path, class: "pe-4 current-events") :
+    type.include?("past") ?
+      (link_to "View current events & exhibits", events_path(anchor: "list"), class: "pe-4 current-events") :
       (link_to "View past events & exhibits", past_events_path, class: "pe-4 past-events")
   end
 
@@ -83,12 +95,14 @@ module EventHelper
 
   def close_button(type)
     case type
-    when "past"
+    when "past_events"
+      link_to "X", past_events_path(anchor: "list"), class: "close-events-search"
+    when "past_workshops"
       link_to "X", past_events_path(anchor: "list"), class: "close-events-search"
     when "past_search"
       link_to "X", past_events_path(anchor: "list"), class: "close-events-search"
     else
-      link_to "X", events_path(anchor: "list"), class: "close-events-search"
+      link_to "X", events_path(anchor: "list"), class: "close-events-search" #, data: { "turbo-frame": "_top" }
     end
   end
 

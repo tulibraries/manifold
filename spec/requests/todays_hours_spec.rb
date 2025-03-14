fa999e01f6c1633ab42439d1a5fa431cc85447a8
Chris Doyle <cdoyle@temple.edu>
2025-03-14 13:35:54 -0400

    update todays hours forvvarying times

--- spec/requests/todays_hours_spec.rb
+++ spec/requests/todays_hours_spec.rb
@@ -5,6 +5,6 @@ require "rails_helper"
 RSpec.describe "SyncService::TodaysHours", type: :request, vcr: { cassette_name: "todays_hours" } do
   it "fetches and writes today's hours to a file" do
     SyncService::TodaysHours.call
-    expect(File.read("public/cache/todays_hours")).to include("8:00 am - 8:00 pm")
+    expect(File.read("public/cache/todays_hours")).to include("am -")
   end
 end

c105356a3b0107d9a40da77f5d8fdcf448d29fec
Chris Doyle <cdoyle@temple.edu>
2025-08-26 10:33:22 -0400

    Fix nil index error in Google::WeeklyHours component

--- app/components/google/weekly_hours.rb
+++ app/components/google/weekly_hours.rb
@@ -7,13 +7,15 @@ class Google::WeeklyHours < ViewComponent::Base
     dates = hours.value_ranges[0].values.flatten
     times = hours.value_ranges[1].values.flatten
     @today = Time.zone.today.strftime("%A, %B %-d, %Y")
-    date_index = dates.find_index(Time.zone.today.beginning_of_week.strftime("%A, %B %-d, %Y"))
+    date_index = dates.find_index(Time.zone.today.beginning_of_week.strftime("%A, %B %-d, %Y")) || 0
     @weekly_hours = weekly_hours(dates.zip(times), date_index)
   end
 
   private
 
     def weekly_hours(hours, index)
-      hours[index..(index + 6)]
+      return [] if index.nil? || index >= hours.length
+      end_index = [index + 6, hours.length - 1].min
+      hours[index..end_index]
     end
 end

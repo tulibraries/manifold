106ac240af38d9f6ebc5e27bc5025e52aa38f99b
Steven Ng <steven.ng@temple.edu>
2025-10-03 16:09:36 -0600

    Undo error to test slack notification

--- config/routes.rb
+++ config/routes.rb
@@ -166,7 +166,7 @@ Rails.application.routes.draw do
 
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
-  match '/503', to: "errors#service_unavailable", via: :all
+  match "/503", to: "errors#service_unavailable", via: :all
 
   controller :redirects do
     get "*path" => :show,

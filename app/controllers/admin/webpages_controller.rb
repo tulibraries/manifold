9adda8e75842fbc407f7fb2631a76e58a9b6b43c
Chris Doyle <cdoyle@temple.edu>
2026-01-06 12:26:27 -0500

    add resource_params method to webpage admin controller to target fileabilities

--- app/controllers/admin/webpages_controller.rb
+++ app/controllers/admin/webpages_controller.rb
@@ -3,5 +3,36 @@
 module Admin
   class WebpagesController < Admin::ApplicationController
     include Admin::Draftable
+
+    def resource_params
+      permitted = super
+      return permitted unless action_name == "update"
+      return permitted unless params[:webpage]&.key?(:file_upload_ids)
+      return permitted unless requested_resource
+
+      selected_ids = Array(permitted[:file_upload_ids]).reject(&:blank?).map(&:to_i)
+      attributes = (permitted[:fileabilities_attributes] || {}).to_h
+      attributes_by_id = {}
+
+      attributes.each do |key, attrs|
+        attrs = attrs.to_h
+        attributes[key] = attrs
+        id = attrs[:id] || attrs["id"]
+        attributes_by_id[id.to_i] = key if id.present?
+      end
+
+      requested_resource.fileabilities.each do |fileability|
+        next if selected_ids.include?(fileability.file_upload_id)
+
+        if (key = attributes_by_id[fileability.id])
+          attributes[key]["_destroy"] = "1"
+        else
+          attributes[attributes.size.to_s] = { id: fileability.id, _destroy: "1" }
+        end
+      end
+
+      permitted[:fileabilities_attributes] = attributes if attributes.any?
+      permitted
+    end
   end
 end

1f27b49cb3d5e80a4a4580da181eb0918c366e1d
Chris Doyle <cdoyle@temple.edu>
2026-01-07 10:19:19 -0500

    add test/update method

--- app/controllers/admin/webpages_controller.rb
+++ app/controllers/admin/webpages_controller.rb
@@ -12,26 +12,25 @@ module Admin
 
       selected_ids = Array(permitted[:file_upload_ids]).reject(&:blank?).map(&:to_i)
       attributes = (permitted[:fileabilities_attributes] || {}).to_h
-      attributes_by_id = {}
+      filtered_attributes = {}
 
       attributes.each do |key, attrs|
         attrs = attrs.to_h
-        attributes[key] = attrs
         id = attrs[:id] || attrs["id"]
-        attributes_by_id[id.to_i] = key if id.present?
-      end
 
-      requested_resource.fileabilities.each do |fileability|
-        next if selected_ids.include?(fileability.file_upload_id)
-
-        if (key = attributes_by_id[fileability.id])
-          attributes[key]["_destroy"] = "1"
-        else
-          attributes[attributes.size.to_s] = { id: fileability.id, _destroy: "1" }
+        if id.present?
+          fileability = requested_resource.fileabilities.find_by(id: id.to_i)
+          next if fileability && !selected_ids.include?(fileability.file_upload_id)
         end
+
+        filtered_attributes[key] = attrs
       end
 
-      permitted[:fileabilities_attributes] = attributes if attributes.any?
+      if filtered_attributes.any?
+        permitted[:fileabilities_attributes] = filtered_attributes
+      else
+        permitted.delete(:fileabilities_attributes)
+      end
       permitted
     end
   end

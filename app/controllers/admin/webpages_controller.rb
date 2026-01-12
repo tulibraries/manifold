d63d8505374e5f21c3a33873ef751df7da81a5e2
Chris Doyle <cdoyle@temple.edu>
2026-01-12 15:30:20 -0500

    MAN-1521-file-removal-bug (#4185)
    The new method adds _destroy for any fileability whose file_upload_id is
    no longer selected, so the deselect now takes effect.

--- app/controllers/admin/webpages_controller.rb
+++ app/controllers/admin/webpages_controller.rb
@@ -4,6 +4,36 @@ module Admin
   class WebpagesController < Admin::ApplicationController
     include Admin::Draftable
 
+    def resource_params
+      permitted = super
+      return permitted unless action_name == "update"
+      return permitted unless params[:webpage]&.key?(:file_upload_ids)
+      return permitted unless requested_resource
+
+      selected_ids = Array(permitted[:file_upload_ids]).reject(&:blank?).map(&:to_i)
+      attributes = (permitted[:fileabilities_attributes] || {}).to_h
+      filtered_attributes = {}
+
+      attributes.each do |key, attrs|
+        attrs = attrs.to_h
+        id = attrs[:id] || attrs["id"]
+
+        if id.present?
+          fileability = requested_resource.fileabilities.find_by(id: id.to_i)
+          next if fileability && !selected_ids.include?(fileability.file_upload_id)
+        end
+
+        filtered_attributes[key] = attrs
+      end
+
+      if filtered_attributes.any?
+        permitted[:fileabilities_attributes] = filtered_attributes
+      else
+        permitted.delete(:fileabilities_attributes)
+      end
+      permitted
+    end
+
     def scoped_resource
       resource = super
       return resource unless current_account&.student?

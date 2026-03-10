0e46bba70c5c7d645f1dc505735b0b3e891e725c
Chris Doyle <cdoyle@temple.edu>
2026-03-10 12:37:37 -0400

    add image dimensions, update concern to supply images with known dimensions

--- app/models/concerns/imageable.rb
+++ app/models/concerns/imageable.rb
@@ -3,6 +3,9 @@
 module Imageable
   extend ActiveSupport::Concern
 
+  FEATURED_IMAGE_WIDTH = 420
+  FEATURED_IMAGE_HEIGHT = 270
+
   included do
     has_one_attached :image, dependent: :destroy
     validates :image, content_type: { in: ["image/png", "image/jpeg", "image/gif"],
@@ -28,29 +31,49 @@ module Imageable
   end
 
   def featured_image()
-    custom_image(200, 140)
+    padded_image(FEATURED_IMAGE_WIDTH, FEATURED_IMAGE_HEIGHT)
   end
 
   def custom_image(width, height)
-    image.analyze unless image.analyzed
+    ensure_image_analyzed
 
-    image_width = (image.metadata[:width].presence || 240)
-    image_height = (image.metadata[:height].presence || 240)
+    return image if image_matches_dimensions?(width, height)
 
-    if (image_width != width) || (image_height != height)
-      if image_width > image_height
-        image.variant(format: :png,
-                      background: :transparent,
-                      gravity: "North",
-                      resize_to_fill: [width, height])
-      else
-        image.variant(format: :png,
-                      background: :transparent,
-                      gravity: :center,
-                      resize_to_fill: [width, height])
-      end
-    else
-      image
-    end
+    image.variant(format: :png,
+                  background: :transparent,
+                  gravity: crop_gravity,
+                  resize_to_fill: [width, height])
   end
+
+  private
+
+    def padded_image(width, height)
+      ensure_image_analyzed
+
+      return image if image_matches_dimensions?(width, height)
+
+      image.variant(format: :png,
+                    background: "#F7F7F7",
+                    gravity: "Center",
+                    resize_to_fit: [width, height],
+                    extent: "#{width}x#{height}")
+    end
+
+    def ensure_image_analyzed
+      image.analyze unless image.analyzed?
+    end
+
+    def image_matches_dimensions?(width, height)
+      image_width = image.metadata[:width].presence || 240
+      image_height = image.metadata[:height].presence || 240
+
+      image_width == width && image_height == height
+    end
+
+    def crop_gravity
+      image_width = image.metadata[:width].presence || 240
+      image_height = image.metadata[:height].presence || 240
+
+      image_width > image_height ? "North" : "Center"
+    end
 end

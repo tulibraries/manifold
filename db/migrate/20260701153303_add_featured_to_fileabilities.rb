7909cb1ebe449b953da173176586fd77dd727d18
Chris Doyle <cdoyle@temple.edu>
2026-07-02 12:23:08 -0400

    rubocop fixes

--- db/migrate/20260701153303_add_featured_to_fileabilities.rb
+++ db/migrate/20260701153303_add_featured_to_fileabilities.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddFeaturedToFileabilities < ActiveRecord::Migration[8.1]
   def change
     add_column :fileabilities, :featured, :boolean, default: false
--- db/migrate/20260701153434_add_featured_to_external_link_webpages.rb
+++ db/migrate/20260701153434_add_featured_to_external_link_webpages.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddFeaturedToExternalLinkWebpages < ActiveRecord::Migration[8.1]
   def change
     add_column :external_link_webpages, :featured, :boolean, default: false

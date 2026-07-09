21ae199c48e45f0c9623e9ecba37fbd234be045a
Chris Doyle <cdoyle@temple.edu>
2026-07-09 12:50:31 -0400

    Point the admin Sync button at the LibCal sync

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -12,10 +12,14 @@ module Admin
     end
 
     def sync
-      SyncService::Events.call(force: true)
+      SyncService::LibcalEvents.call
       @message = Rails.cache.read("events_image_error")
       flash[:notice] = @message ? @message : "Events synced"
       redirect_to admin_events_path
+    end
+
+    def valid_action?(name, resource = resource_class)
+      %w[new destroy].exclude?(name.to_s) && super
     end
   end
 end

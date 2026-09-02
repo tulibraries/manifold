685b4539b0f002a0f4936928b631e59d0d27eb8e
Chris Doyle <cdoyle@temple.edu>
2026-09-02 14:20:17 -0400

    add view test for featured image behavior

--- spec/views/exhibitions/show.html.erb_spec.rb
+++ spec/views/exhibitions/show.html.erb_spec.rb
@@ -17,6 +17,29 @@ RSpec.describe "exhibitions/show", type: :view do
     expect(rendered).to match /#{@exhibition.images.first.blob.filename}/
   end
 
+  it "renders the featured image with its alt text" do
+    @exhibition = FactoryBot.create(:exhibition, :with_processed_image,
+                                    alt_text: "Dali melting clocks")
+    render
+
+    expect(rendered).to have_css("div.exhibition-featured-image img")
+    expect(rendered).to include('alt="Dali melting clocks"')
+  end
+
+  it "falls back to a title-based alt text on the featured image" do
+    @exhibition = FactoryBot.create(:exhibition, :with_processed_image, alt_text: nil)
+    render
+
+    expect(rendered).to include('alt="Exhibition image for Salvador Dali"')
+  end
+
+  it "omits the featured image container when no image is attached" do
+    @exhibition = FactoryBot.create(:exhibition)
+    render
+
+    expect(rendered).not_to have_css("div.exhibition-featured-image")
+  end
+
   it "renders script for exhibition" do
     @exhibition = FactoryBot.create(:exhibition, online_url: "https://library.temple.edu")
     render
